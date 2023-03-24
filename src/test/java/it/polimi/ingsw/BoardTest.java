1e7007fedd8d5f0711d57bea71966d207d681693
Giacomo Groppi <giamg01@gmail.com>
2023-03-24 15:09:25 +0100

    Move from JUnit 4 to JUnit 5

--- src/test/java/it/polimi/ingsw/BoardTest.java
+++ src/test/java/it/polimi/ingsw/BoardTest.java
@@ -5,15 +5,13 @@ import it.polimi.ingsw.model.Board;
 
 import it.polimi.ingsw.model.IllegalExtractionException;
 import it.polimi.ingsw.model.Tile;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.Assert;
+import org.junit.jupiter.api.*;
 
 import java.util.List;
 
 public class BoardTest {
     private Board board;
-    @Before
+    @BeforeEach
     public void setUp() {
         board = new Board();
     }
@@ -24,33 +22,33 @@ public class BoardTest {
         Bag bag = new Bag();
 
         for (int i = 0; i < 33; i++) {
-            Assert.assertFalse(board.isFull(2));
+            Assertions.assertFalse(board.isFull(2));
             board.fillRandomly(bag.getRandomTile(), 2);
             if (printBoard) System.out.print(board);
 
         }
 
-        Assert.assertTrue(board.isFull(2));
-        Assert.assertFalse(board.isFull(3));
-        Assert.assertFalse(board.isFull(4));
+        Assertions.assertTrue(board.isFull(2));
+        Assertions.assertFalse(board.isFull(3));
+        Assertions.assertFalse(board.isFull(4));
 
         for (int i = 0; i < 7; i++) {
-            Assert.assertFalse(board.isFull(3));
+            Assertions.assertFalse(board.isFull(3));
             board.fillRandomly(bag.getRandomTile(), 3);
             if (printBoard) System.out.print(board);
         }
 
-        Assert.assertTrue(board.isFull(3));
-        Assert.assertFalse(board.isFull(4));
+        Assertions.assertTrue(board.isFull(3));
+        Assertions.assertFalse(board.isFull(4));
 
         for (int i = 0; i < 5; i++) {
-            Assert.assertFalse(board.isFull(4));
+            Assertions.assertFalse(board.isFull(4));
             board.fillRandomly(bag.getRandomTile(), 4);
             if (printBoard) System.out.print(board);
 
         }
 
-        Assert.assertTrue(board.isFull(4));
+        Assertions.assertTrue(board.isFull(4));
     }
 
     static void fillBoard(Board board) {
@@ -60,45 +58,52 @@ public class BoardTest {
             board.fillRandomly(bag.getRandomTile(), 4);
         }
 
-        Assert.assertTrue(board.isFull(4));
+        Assertions.assertTrue(board.isFull(4));
     }
 
     @Test
     public void testSelectAvailableTiles() throws IllegalExtractionException {
         fillBoard(board);
 
-        Assert.assertEquals(20, board.getSelectableTiles().size());
+        Assertions.assertEquals(20, board.getSelectableTiles().size());
 
         board.selectTile(4, 0);
 
-        Assert.assertEquals( 2, board.getSelectableTiles().size());
+        Assertions.assertEquals( 2, board.getSelectableTiles().size());
 
         board.selectTile(4, 1);
 
-        Assert.assertEquals(1, board.getSelectableTiles().size());
+        Assertions.assertEquals(1, board.getSelectableTiles().size());
 
         board.selectTile(4, 2);
     }
 
-    @Test (expected = IllegalExtractionException.class)
+    @Test
     public void testWrongeSelectAfterOne() throws IllegalExtractionException {
         fillBoard(this.board);
-        board.selectTile(5, 5);
+
+        Assertions.assertThrows(IllegalExtractionException.class, () -> {
+            board.selectTile(5, 5);
+        });
     }
 
     @Test (expected = IllegalExtractionException.class)
     public void testWrongeSelectAfterTwo() throws IllegalExtractionException {
         fillBoard(this.board);
         board.selectTile(5, 1);
-        board.selectTile(4, 2);
+        Assertions.assertThrows(IllegalExtractionException.class, () -> {
+            board.selectTile(4, 2);
+        });
     }
 
-    @Test (expected = IllegalExtractionException.class)
+    @Test
     public void testWrongeSelectAfterThree() throws IllegalExtractionException {
         fillBoard(this.board);
         board.selectTile(7, 3);
         board.selectTile(6, 3);
-        board.selectTile(6, 4);
+        Assertions.assertThrows(IllegalExtractionException.class, () -> {
+            board.selectTile(6, 4);
+        });
     }
 
     @Test
@@ -121,7 +126,7 @@ public class BoardTest {
     public void testSelectDrawOne() throws IllegalExtractionException {
         fillBoard(this.board);
         board.selectTile(7, 3);
-        Assert.assertEquals(1, board.draw().size());
+        Assertions.assertEquals(1, board.draw().size());
     }
 
     @Test
@@ -129,7 +134,7 @@ public class BoardTest {
         fillBoard(this.board);
         board.selectTile(7, 3);
         board.selectTile(6, 3);
-        Assert.assertEquals(2, board.draw().size());
+        Assertions.assertEquals(2, board.draw().size());
     }
 
     @Test
@@ -138,6 +143,26 @@ public class BoardTest {
         board.selectTile(7, 3);
         board.selectTile(6, 3);
         board.selectTile(5, 3);
-        Assert.assertEquals(3, board.draw().size());
+        Assertions.assertEquals(3, board.draw().size());
+    }
+
+    @Test
+    public void testNeedRefill() throws IllegalExtractionException {
+        fillBoard(this.board);
+        Assertions.assertFalse(board.needsRefill());
+
+        board.selectTile(5, 1);
+        board.selectTile(5, 2);
+        board.selectTile(5, 3);
+
+        Assertions.assertEquals(3, board.draw().size());
+        Assertions.assertFalse(board.needsRefill());
+
+        board.selectTile(4, 0);
+        board.selectTile(4, 1);
+
+        Assertions.assertEquals(2, board.draw().size());
+
+        Assertions.assertTrue(board.needsRefill());
     }
 }
---
+++ src/test/java/it/polimi/ingsw/model/BagTest.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BagTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
