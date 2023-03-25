55c1177e83dd5d68a9b1bc443e7d2e50f9bcb962
Cristiano Migali <cristiano.migali@gmail.com>
2023-03-25 15:48:08 +0100

    Added JavaDoc documentation to LibraryMask

--- src/main/java/it/polimi/ingsw/model/LibraryMask.java
+++ src/main/java/it/polimi/ingsw/model/LibraryMask.java
@@ -3,18 +3,42 @@ package it.polimi.ingsw.model;
 import java.util.ArrayList;
 import java.util.function.BiFunction;
 
+/**
+ * Represents a mutable set of {@link Shelf shelves} inside a {@link Library library}.
+ */
 public class LibraryMask {
+    /**
+     * The library this mask is referred to.
+     */
     private final Library library;
+
+    /**
+     * The {@link Shelf shelves} in the set.
+     */
     private final ArrayList<Shelf> shelves = new ArrayList<>();
 
+    /**
+     * Constructor of the class.
+     *
+     * @param library is the library the created mask is referred to.
+     */
     public LibraryMask(Library library) {
         this.library = library;
     }
 
+    /**
+     * Removes all the {@link Shelf shelves} from the set.
+     */
     public void clear() {
         shelves.clear();
     }
 
+    /**
+     * Adds a {@link Shelf shelf} to the set.
+     *
+     * @param shelf is the shelf that we are going to add to the set.
+     * @throws RuntimeException if the shelf is already in the set.
+     */
     public void add(Shelf shelf) {
         for (int i = 0; i < shelves.size(); i++) {
             if (shelf.equals(shelves.get(i))) {
@@ -29,13 +53,29 @@ public class LibraryMask {
 
         shelves.add(shelf);
     }
+
+    /**
+     * @return all the {@link Shelf shelves} inside the set.
+     */
     public ArrayList<Shelf> getShelves() {
         return new ArrayList<>(shelves);
     }
-    // gets the first tile in the mask
+
+    /**
+     * @return the tile inside a shelf in the set.
+     *
+     * @author Michele Miotti
+     */
     public Tile getSampleTile() {
         return library.get(shelves.get(0));
     }
+
+    /**
+     * Lambda function which returns true iff the two library masks in input do not intersect, that is the intersection
+     * of their sets is empty.
+     *
+     * @author Michele Miotti
+     */
     public static final BiFunction<LibraryMask, LibraryMask, Boolean> DO_NOT_INTERSECT = (first, second) -> {
         for (Shelf A : first.shelves) {
             for (Shelf B : second.shelves) {
@@ -47,6 +87,14 @@ public class LibraryMask {
         return true;
     };
 
+    /**
+     * Lambda function which returns true iff the two library masks in input do not intersect, that is the intersection
+     * of their sets is empty, and all the tiles contained in shelves inside a mask or the other are of the same color.
+     * A precondition of this function is that all the tiles contained in shelves inside the same mask must be of the
+     * same color.
+     *
+     * @author Michele Miotti
+     */
     public static final BiFunction<LibraryMask, LibraryMask, Boolean> DO_NOT_INTERSECT_AND_SAME_COLOR = (first, second) ->
         first.getSampleTile() == second.getSampleTile() && DO_NOT_INTERSECT.apply(first, second);
 
