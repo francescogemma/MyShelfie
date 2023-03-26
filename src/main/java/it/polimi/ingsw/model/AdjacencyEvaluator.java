3008b4f3f0586aee74b829eaf5059311228087ae
michele-miotti-uni <michele.miotti@mail.polimi.it>
2023-03-26 16:25:57 +0200

    Added documentation for the AdjacencyEvaluator type.

--- src/main/java/it/polimi/ingsw/model/AdjacencyEvaluator.java
+++ src/main/java/it/polimi/ingsw/model/AdjacencyEvaluator.java
@@ -1,19 +1,36 @@
 package it.polimi.ingsw.model;
 
-import java.util.ArrayList;
-
+/**
+ * Reads {@link LibraryMask a LibaryMask} and notes down its score
+ * according to the game rules, based on its size.
+ *
+ * @author Michele Miotti
+ */
 public class AdjacencyEvaluator implements Evaluator {
-    private LibraryMaskSet libraryMaskSet;
     private int points;
+    /**
+     * Object constructor. Sets the current amount of points to 0.
+     */
     public AdjacencyEvaluator() {
         points = 0;
     }
+    /**
+     * Add a {@link LibraryMask LibraryMask} and evaluate it.
+     * @param libraryMask will be taken account for, and will have an impact on the current score,
+     *                    if its size is greater than 2.
+     * @return false every time, since it's always possible to gather new points from any state.
+     */
     public boolean add(LibraryMask libraryMask) {
         points = convertSizeToPoints(libraryMask.getShelves().size());
         // always return false since it's always possible to get new points
         return false;
     }
-
+    /**
+     * Returns the correct amount of points relative to the size of some
+     * shelf cluster, following the amounts specified in the game rules.
+     * @param size indicates the amount of adjacent shelves in a mask.
+     * @return the correct score relative to the input size.
+     */
     private int convertSizeToPoints(int size) {
         if (size >= 6) return 8;
         if (size == 5) return 5;
@@ -21,6 +38,10 @@ public class AdjacencyEvaluator implements Evaluator {
         if (size == 3) return 2;
         return 0;
     }
+
+    /**
+     * Gets the points gathered so far.
+     */
     public int getPoints() {
         return points;
     }
