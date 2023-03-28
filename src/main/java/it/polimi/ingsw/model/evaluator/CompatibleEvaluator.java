bb086839f4686de9c782637c2624441baad9b59d
michele-miotti-uni <michele.miotti@mail.polimi.it>
2023-03-28 15:04:48 +0200

    CompatibleEvaluator now extends CommonGoalEvaluator, and correctly use its getPoints method.

--- src/main/java/it/polimi/ingsw/model/evaluator/CompatibleEvaluator.java
+++ src/main/java/it/polimi/ingsw/model/evaluator/CompatibleEvaluator.java
@@ -14,19 +14,13 @@ import java.util.function.BiPredicate;
  *
  * @author Michele Miotti
  */
-public class CompatibleEvaluator implements Evaluator {
+public class CompatibleEvaluator extends CommonGoalEvaluator implements Evaluator {
     /**
      * This ArrayList contains {@link BookshelfMaskSet BookshelfMaskSets}.
      * It represents the set of all possible compatible sets that can be made
      * with all {@link BookshelfMask masks} given so far.
      */
     private final ArrayList<BookshelfMaskSet> group;
-
-    /**
-     * If the target is met, the player will receive these points,
-     * otherwise, they will receive zero points.
-     */
-    private final int points;
 
     /**
      * This is how big the biggest set in our group needs to be in order
@@ -49,18 +43,17 @@ public class CompatibleEvaluator implements Evaluator {
 
     /**
      * Class constructor.
-     * @param points are returned by the {@link CompatibleEvaluator#getPoints() getPoints}
-     *               method if the {@link CompatibleEvaluator#targetSetSize target} has been met.
+     * @param playersAmount is this game's player amount (2 to 4).
      * @param targetGroupSize is the cluster size to reach in order to allow the
      *                        {@link CompatibleEvaluator#getPoints() getPoints} method to return a non-zero result.
      * @param compatible is a BiPredicate that defines the meaning of "compatibility" within elements of a set.
      *                   Said {@link BookshelfMaskSet sets} are stored in the {@link CompatibleEvaluator#group} variable.
      */
-    public CompatibleEvaluator(int points, int targetGroupSize, BiPredicate<BookshelfMask, BookshelfMask> compatible) {
+    public CompatibleEvaluator(int playersAmount, int targetGroupSize, BiPredicate<BookshelfMask, BookshelfMask> compatible) {
+        super(playersAmount);
         group = new ArrayList<>();
 
         this.compatible = compatible;
-        this.points = points;
         this.targetSetSize = targetGroupSize;
         this.targetMet = targetSetSize <= 1;
     }
@@ -76,9 +69,7 @@ public class CompatibleEvaluator implements Evaluator {
 
                 // note down if this addition meets out target size
                 targetMet |= bookshelfMaskSet.getSize() >= targetSetSize;
-                if (targetMet) {
-                    return true;
-                }
+                if (targetMet) { return true; }
             }
         }
         // create and add one more set with a single BookshelfMask
@@ -91,7 +82,7 @@ public class CompatibleEvaluator implements Evaluator {
 
     @Override
     public int getPoints() {
-        if (targetMet) { return points; }
+        if (targetMet) { return super.getPoints(); }
         return 0;
     }
 }
