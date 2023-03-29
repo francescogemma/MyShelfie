064b3dfde34c33f21071da787f2b855836eb9330
Giacomo Groppi <giamg01@gmail.com>
2023-03-29 18:32:47 +0200

    Added Row Different Color goal

---
+++ src/main/java/it/polimi/ingsw/model/goal/RowFiveAllDifferentColor.java
@@ -0,0 +1,20 @@
+package it.polimi.ingsw.model.goal;
+
+import it.polimi.ingsw.model.bookshelf.Shape;
+import it.polimi.ingsw.model.evaluator.AtLeastEvaluator;
+import it.polimi.ingsw.model.fetcher.ShapeFetcher;
+import it.polimi.ingsw.model.filter.NumDifferentColorFilter;
+
+/**
+ * Two rows, each consisting of 5 different types of tiles.
+ * @author Giacomo Groppi
+ * */
+public class RowFiveAllDifferentColor extends CommonGoal{
+    protected RowFiveAllDifferentColor(int numPlayer) {
+        super(
+                new ShapeFetcher(Shape.getRow(5)),
+                new NumDifferentColorFilter(5, 5),
+                new AtLeastEvaluator(numPlayer, 2, bookshelfMask -> bookshelfMask.getShelves().size() >= 2)
+        );
+    }
+}
