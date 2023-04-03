08ae5aff3901e72616e213dff5c0d4d8f71d49de
Francesco Gemma <fgemma26@gmail.com>
2023-04-03 18:52:07 +0200

    Added mask system to Goal class

--- src/main/java/it/polimi/ingsw/model/goal/Goal.java
+++ src/main/java/it/polimi/ingsw/model/goal/Goal.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.model.goal;
 
 import it.polimi.ingsw.model.bookshelf.Bookshelf;
 import it.polimi.ingsw.model.bookshelf.BookshelfMask;
+import it.polimi.ingsw.model.bookshelf.BookshelfMaskSet;
 import it.polimi.ingsw.model.bookshelf.Shelf;
 import it.polimi.ingsw.model.evaluator.Evaluator;
 import it.polimi.ingsw.model.fetcher.Fetcher;
@@ -11,11 +12,13 @@ public abstract class Goal {
     private final Fetcher fetcher;
     private final Filter filter;
     private final Evaluator evaluator;
+    private BookshelfMaskSet pointMasks;
 
     protected Goal(Fetcher fetcher, Filter filter, Evaluator evaluator) {
         this.fetcher = fetcher;
         this.filter = filter;
         this.evaluator = evaluator;
+        pointMasks = new BookshelfMaskSet((a, b) -> true);
     }
 
     public final int calculatePoints(Bookshelf bookshelf) {
@@ -46,9 +49,14 @@ public abstract class Goal {
         filter.clear();
 
         try {
+            pointMasks = evaluator.getPointMasks();
             return evaluator.getPoints();
         } finally {
             evaluator.clear();
         }
+    }
+
+    public final BookshelfMaskSet getPointMasks() {
+        return pointMasks;
     }
 }
