0e04b2cc0cd628277b3a93489fcc30209ff3d94b
giacomogroppi <giamg01@gmail.com>
2023-04-07 14:13:09 +0200

    Added some functions to GameState

--- src/main/java/it/polimi/ingsw/model/game/GameState.java
+++ src/main/java/it/polimi/ingsw/model/game/GameState.java
@@ -1,10 +1,21 @@
 package it.polimi.ingsw.model.game;
 
+import it.polimi.ingsw.model.Tile;
+import it.polimi.ingsw.model.board.IllegalExtractionException;
+import it.polimi.ingsw.model.board.SelectionFullException;
 import it.polimi.ingsw.utils.Coordinate;
 
 import java.util.Collection;
 import java.util.Optional;
 
+/**
+ * GameState defines the base class of each game state.
+ * It contains all methods invoked by Game.
+ * Each function if not redefined throws an IllegalFlowException exception to
+ *  indicate that the function call is not legal due to the defined state.
+ *
+ * @author Giacomo Groppi
+ * */
 abstract class GameState{
     protected GameData gameData;
 
@@ -12,13 +23,20 @@ abstract class GameState{
         this.gameData = data;
     }
 
+    /**
+     * Add player to the game
+     * @throws IllegalFlowException iff the game has already started
+     * */
     @SuppressWarnings("unused")
-    protected void addPlayer(Player ignored) throws IllegalFlowException {
+    protected void addPlayer(Player player) throws IllegalFlowException {
         throw new IllegalFlowException();
     }
 
+    /**
+     *
+     * */
     @SuppressWarnings("unused")
-    protected void selectTile (Player player, Collection<Coordinate> coordinate, int col) throws IllegalFlowException {
+    protected void selectTile (Player player, Collection<Coordinate> coordinate, int col) throws IllegalFlowException, IllegalExtractionException, SelectionFullException {
         throw new IllegalFlowException();
     }
 
@@ -42,6 +60,7 @@ abstract class GameState{
     boolean isOver () {
         return this.gameData.winner.isPresent();
     }
+
     boolean isConnected (Player p) throws PlayerNotInGameException {
         final int index = this.indexOf(p);
         return this.gameData.players.get(index).getValue();
@@ -81,6 +100,47 @@ abstract class GameState{
             throw new IllegalArgumentException("Winner already set");
 
         this.gameData.winner = Optional.of(player);
+    }
+
+    /**
+     * The function returns a winner only if the game is over.
+     * @throws IllegalFlowException iff game is not over jet.
+     * */
+    public Player getWinner() throws IllegalFlowException {
+        throw new IllegalFlowException();
+    }
+
+    protected boolean containsPlayer (final Player player) {
+        return this.gameData.players
+                .stream()
+                .anyMatch(p -> p.getKey().equals(player));
+    }
+
+    public Collection<Coordinate> getSelectableTile () throws IllegalFlowException {
+        throw new IllegalFlowException();
+    }
+
+    protected void fillBagIfNecessary() {
+        if (!this.gameData.board.needsRefill()) {
+            return;
+        }
+
+        final int n = this.gameData.players.size();
+        for (;;) {
+            Tile t;
+            try {
+                t = this.gameData.bag.getRandomTile();
+            } catch (Exception e) {
+                break;
+            }
+
+            try {
+                this.gameData.board.fillRandomly(t, n);
+            } catch (IllegalArgumentException e) {
+                this.gameData.board.forgetSelection();
+                break;
+            }
+        }
     }
 
     @Override
