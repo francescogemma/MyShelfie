3736e9a2c7fd21e944edffbd22d7c6a189285609
giacomogroppi <giamg01@gmail.com>
2023-04-13 20:46:41 +0200

    Added javadoc to Game & Coordinate

--- src/main/java/it/polimi/ingsw/model/game/Game.java
+++ src/main/java/it/polimi/ingsw/model/game/Game.java
@@ -7,23 +7,72 @@ import it.polimi.ingsw.model.goal.PersonalGoal;
 
 import java.util.*;
 
+/**
+ * Class for manage game.
+ * @author Giacomo Groppi
+ * @author Cristiano Migali
+ * */
 public class Game {
     // TODO: Add JavaDoc for this class
 
+    /*
+     * The index of the first player in the list of players.
+     */
     private static final int FIRST_PLAYER_INDEX = 0;
+
+    /*
+     * The name of the game.
+     */
     private final String name;
+
+    /*
+     * The list of players in the game.
+     */
     private final List<Player> players;
+
+    /*
+     * The optional winner of the game.
+     */
     private Optional<Player> winner;
+
+    /**
+     * The common goals of the game.
+     * */
     private CommonGoal[] commonGoals;
+
+    /**
+     * The bag of tiles in the game.
+     */
     private final Bag bag;
+
+    /**
+     * The board of the game.
+     */
     private final Board board;
+
+    /**
+     * Whether the game has started.
+     */
     private boolean isStarted;
+
     private List<Integer> personalGoalIndexes;
     private int personalGoalIndex;
 
+    /**
+     * The index of the current player in the list of players.
+     */
     private int currentPlayerIndex;
 
+    /**
+     * Creates a new game with the given name.
+     * @param name the name of the game
+     * @throws IllegalArgumentException iff the name is empty
+     * @throws NullPointerException iff name is null
+     */
     public Game(String name) {
+        if (name == null)
+            throw new NullPointerException();
+
         if (name.length() == 0)
             throw new IllegalArgumentException("String is empty");
 
@@ -43,6 +92,11 @@ public class Game {
         this.personalGoalIndex = 0;
     }
 
+    /**
+     * Gets the starting player of the game.
+     * @return the starting player
+     * @throws IllegalFlowException if there are no players in the game
+     */
     public Player getStartingPlayer() throws IllegalFlowException {
         if (players.isEmpty()) {
             throw new IllegalFlowException("There is no starting player until someone joins the game");
@@ -51,11 +105,33 @@ public class Game {
         return players.get(FIRST_PLAYER_INDEX);
     }
 
+    /**
+     * Checks if the game is over.
+     * @return true iff the game is over, false otherwise
+     */
     public boolean isOver () {
         return winner.isPresent();
     }
 
+    /**
+     * Adds a player to the game with the given username.
+     * @param username the username of the player to add
+     * @throws PlayerAlreadyInGameException if the player is already in the game
+     * @throws NullPointerException
+     *  <ul>
+     *      <li> username is null </li>
+     *      <li> a player with the name "username" already exists </li>
+     *  </ul>
+     * @throws IllegalFlowException
+     *  <ul>
+     *      <li> if the game has already started </li>
+     *      <li> if the game is already 4 players </li>
+     *  </ul>
+     */
     public void addPlayer(String username) throws IllegalFlowException, PlayerAlreadyInGameException {
+        if (username == null || username.length() == 0)
+            throw new NullPointerException("username is null or has length 0");
+
         // Allows for reconnection of disconnected players
         for (Player otherPlayer : players) {
             if (otherPlayer.getUsername().equals(username)) {
@@ -83,6 +159,11 @@ public class Game {
         players.add(player);
     }
 
+    /**
+     * Starts the game by initializing the common goals and the first player index.
+     *
+     * @throws IllegalFlowException if there are less than two players.
+     */
     public void startGame() throws IllegalFlowException {
         if (players.size() < 2) {
             throw new IllegalFlowException("You need at least two players in order to start the game");
@@ -96,12 +177,18 @@ public class Game {
     }
 
     /**
-     * @return Name of the game.
+     * @return the name of the game.
      */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Returns the current {@link Player player}  of the game.
+     *
+     * @throws IllegalFlowException if the game is not started or if the game is over.
+     * @return the current player of the game.
+     */
     public Player getCurrentPlayer() throws IllegalFlowException {
         if (!isStarted) {
             throw new IllegalFlowException("There is no current player until you start the game");
@@ -114,6 +201,9 @@ public class Game {
         return players.get(currentPlayerIndex);
     }
 
+    /**
+     * @return whether the game is over or not.
+     */
     public Player getWinner () throws IllegalFlowException {
         if (!isOver()) {
             throw new IllegalFlowException("There is no winner until the game is over");
@@ -122,6 +212,15 @@ public class Game {
         return winner.get();
     }
 
+    /**
+     * The function ends the current player's turn and moves on to the next one
+     *
+     * @throws IllegalFlowException iff the game hasn't started yet.
+     * <ul>
+     *     <li> the game hasn't started yet </li>
+     *     <li> game is over </li>
+     * </ul>
+     * */
     public void nextTurn() throws IllegalFlowException {
         if (!isStarted) {
             throw new IllegalFlowException("You can't move to next turn until the game has started");
@@ -134,6 +233,10 @@ public class Game {
         currentPlayerIndex = (currentPlayerIndex + 1) % players.size();
     }
 
+    /**
+     * @throws IllegalFlowException iff the game hasn't started yet.
+     * @return the last player in the list of players
+     * */
     public Player getLastPlayer() throws IllegalFlowException {
         if (!isStarted) {
             throw new IllegalFlowException("There is no last player until you start the game");
@@ -142,27 +245,57 @@ public class Game {
         return players.get(players.size() - 1);
     }
 
+
+    /**
+     * @return The list of players in this game.
+     * */
     public List<Player> getPlayers() {
         return new ArrayList<>(players);
     }
 
+    /**
+     * @return The {@link Bag bag} of the current Game
+     * @see Bag
+     * */
     public Bag getBag() {
         return bag;
     }
 
+    /**
+     * @return The {@link Board board} of the current Game
+     * @see Board
+     * */
     public Board getBoard() {
         return board;
     }
 
+    /**
+     * @return The array of all common goals
+     * @see CommonGoal
+     * */
     public CommonGoal[] getCommonGoals() {
         return commonGoals;
     }
 
+    /**
+     * @return true iff at least one bookshelf is full
+     * */
     public boolean atLeastOneBookshelfIsFull() {
         return players.stream().anyMatch(p -> p.getBookshelf().isFull());
     }
 
-    public void setWinner(Player player) {
+
+    /**
+     * @param player winner of the game
+     * @throws IllegalArgumentException iff player is not in this game
+     * @throws IllegalFlowException iff winner is already set
+     * @see Player
+     * */
+    public void setWinner(Player player) throws IllegalFlowException {
+        if (!this.players.contains(player))
+            throw new IllegalArgumentException(player + " is not in this game" + this);
+        if (this.winner.isPresent())
+            throw new IllegalFlowException("Winner is already set. CurrentWinner: [" + this.winner.get() + "]" + " passed winner: [" + player + "]");
         winner = Optional.of(player);
     }
 }
--- src/main/java/it/polimi/ingsw/utils/Coordinate.java
+++ src/main/java/it/polimi/ingsw/utils/Coordinate.java
@@ -1,44 +1,103 @@
 package it.polimi.ingsw.utils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Collection;
 
+/**
+ * The class represents a pair of row and column integers.
+ * @author Giacomo Groppi
+ * */
 public class Coordinate {
+    /*
+     * The column value of the coordinate.
+     */
     private final int col;
+    /*
+     * The row value of the coordinate.
+     * */
     private final int row;
 
+    /**
+     * Constructs a new Coordinate with the given row and column values.
+     * @param row the row value of the coordinate
+     * @param col the col value of the coordinate
+     * */
     public Coordinate(int row, int col) {
         this.row = row;
         this.col = col;
     }
 
+    /**
+     * Converts a collection of integer arrays into a list of Coordinates
+     * Each integer array should have two elements, representing the row and column values.
+     *
+     * @param data the collection of integer arrays to convert
+     * @throws IllegalArgumentException iff exists an element with lenght other than 2
+     * @return a list of Coordinates
+     */
     public static List<Coordinate> toList(Collection<int []> data) {
-        return data
-                .stream()
-                .map(p -> new Coordinate(p[0], p[1]))
-                .toList();
+        List<Coordinate> c = new ArrayList<>();
+        for (int[] d : data) {
+            if (d.length != 2)
+                throw new IllegalArgumentException("Illegal argument number for " + Arrays.toString(d));
+            c.add(new Coordinate(d[0], d[1]));
+        }
+
+        return c;
     }
 
+    /**
+     * Gets the column value of the coordinate.
+     *
+     * @return the column value
+     * */
     public int getCol() {
         return this.col;
     }
 
+    /**
+     * Gets the row value of the coordinate.
+     *
+     * @return the row value
+     * */
     public int getRow() {
         return this.row;
     }
 
+    /**
+     * Returns a new Coordinate that represents the position below this one.
+     *
+     * @return a new Coordinate representing the position below this one
+     * */
     public Coordinate down () {
         return new Coordinate(this.row + 1, col);
     }
 
+    /**
+     * Returns a new Coordinate that represents the position to the right of this one.
+     *
+     * @return a new Coordinate representing the position to the right of this one
+     * */
     public Coordinate right () {
         return new Coordinate(this.row, col + 1);
     }
 
+    /**
+     * Returns a new Coordinate that represents the position above this one.
+     *
+     * @return a new Coordinate representing the position above this one
+     * */
     public Coordinate top () {
         return new Coordinate(this.row - 1, this.col);
     }
 
+    /**
+     * Returns a new Coordinate that represents the position to the left of this one.
+     *
+     * @return a new Coordinate representing the position to the left of this one
+     * */
     public Coordinate left () {
         return new Coordinate(this.row, this.col - 1);
     }
