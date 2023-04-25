436235a444c29197d06f6172e1899b83f9f149d0
Cristiano Migali <cristiano.migali@gmail.com>
2023-04-25 14:45:02 +0200

    Added db to gitignore Added missing GameHasBeenCreated id

--- src/main/java/it/polimi/ingsw/event/EventDataTypeAdapterFactory.java
+++ src/main/java/it/polimi/ingsw/event/EventDataTypeAdapterFactory.java
@@ -36,7 +36,8 @@ public class EventDataTypeAdapterFactory implements TypeAdapterFactory {
             Map.entry(CreateNewGameEventData.ID, CreateNewGameEventData.class),
             Map.entry(InsertTileEventData.ID, InsertTileEventData.class),
             Map.entry(SelectTileEventData.ID, SelectTileEventData.class),
-            Map.entry(DeselectTileEventData.ID, DeselectTileEventData.class)
+            Map.entry(DeselectTileEventData.ID, DeselectTileEventData.class),
+            Map.entry(GameHasBeenCreatedEventData.ID, GameHasBeenCreatedEventData.class)
     );
 
     private static final Map<String, Type> WRAPPER_DATA_TYPES = Map.of(
