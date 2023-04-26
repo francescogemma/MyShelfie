0a24829d87351c1dc31c2f76fcc08d6143d215d0
michele-miotti-uni <michele.miotti@mail.polimi.it>
2023-04-26 18:35:39 +0200

    Added ClientRMIScanner for testing

---
+++ src/main/java/it/polimi/ingsw/networking/example01/ClientRMIScanner.java
@@ -0,0 +1,35 @@
+package it.polimi.ingsw.networking.example01;
+
+import it.polimi.ingsw.networking.Connection;
+import it.polimi.ingsw.networking.DisconnectedException;
+import it.polimi.ingsw.networking.RMI.RMIConnection;
+import it.polimi.ingsw.networking.TCP.TCPConnection;
+
+import java.util.Scanner;
+
+public class ClientRMIScanner {
+    public static void main(String[] args) {
+        final String ADDRESS = "localhost";
+        final int PORT = 1099;
+        Connection connection;
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            connection = new RMIConnection(ADDRESS, PORT);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        try {
+            System.out.println("received: " + connection.receive());
+            System.out.print("Insert a string to send to the server: ");
+            connection.send(scanner.nextLine());
+        } catch (DisconnectedException e) {
+            e.printStackTrace();
+            System.out.println("disconnected");
+        }
+
+        connection.disconnect();
+    }
+}
