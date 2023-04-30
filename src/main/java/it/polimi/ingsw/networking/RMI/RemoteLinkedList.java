237ef8e1b5128eced6582544825d0776ed2e6315
michele-miotti-uni <michele.miotti@mail.polimi.it>
2023-04-30 14:13:23 +0200

    Added missing documentation for RemoteLinkedList and RemoteQueue

--- src/main/java/it/polimi/ingsw/networking/RMI/RemoteLinkedList.java
+++ src/main/java/it/polimi/ingsw/networking/RMI/RemoteLinkedList.java
@@ -5,9 +5,26 @@ import java.rmi.server.UnicastRemoteObject;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * Simple thread-safe LinkedList that can be accessed through remote method invocation.
+ *
+ * @author Michele Miotti
+ */
 public class RemoteLinkedList extends UnicastRemoteObject implements RemoteQueue {
+    /**
+     * (Rep) This queue will be encapsulated by the class itself.
+     */
     private final Queue<String> stringQueue;
+
+    /**
+     * Lock used to handle multiple threads adding and polling concurrently.
+     */
     private final Object lock = new Object();
+
+    /**
+     * Class constructor.
+     * @throws RemoteException will be thrown in case of network problems, or server communication issues.
+     */
     public RemoteLinkedList() throws RemoteException {
         stringQueue = new LinkedList<>();
     }
--- src/main/java/it/polimi/ingsw/networking/RMI/RemoteQueue.java
+++ src/main/java/it/polimi/ingsw/networking/RMI/RemoteQueue.java
@@ -2,7 +2,24 @@ package it.polimi.ingsw.networking.RMI;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+
+/**
+ * Remote interface that supports queue addition and queue polling.
+ *
+ * @author Michele Miotti
+ */
 public interface RemoteQueue extends Remote {
+    /**
+     * Adds a string to the queue.
+     * @param string will be added to the queue.
+     * @throws RemoteException will be thrown in case of network problems, or server communication issues.
+     */
     void add(String string) throws RemoteException;
+
+    /**
+     * Polls a string to the queue (the oldest added string), removing it.
+     * @return the most old element in the queue will be removed and returned
+     * @throws RemoteException will be thrown in case of network problems, or server communication issues.
+     */
     String poll() throws RemoteException;
 }
