82ffd6b2a2660cf68e60b05fb1a4a2223994900f
Cristiano Migali <cristiano.migali@gmail.com>
2023-05-06 17:20:27 +0200

    Now text in pop ups is justified

--- src/main/java/it/polimi/ingsw/view/tui/terminal/drawable/PopUpDrawable.java
+++ src/main/java/it/polimi/ingsw/view/tui/terminal/drawable/PopUpDrawable.java
@@ -3,6 +3,9 @@ package it.polimi.ingsw.view.tui.terminal.drawable;
 import it.polimi.ingsw.view.tui.terminal.drawable.menu.value.TextBox;
 import it.polimi.ingsw.view.tui.terminal.drawable.twolayers.TwoLayersDrawable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PopUpDrawable extends FixedLayoutDrawable<TwoLayersDrawable> {
     private final BlurrableDrawable blurrableBackground;
     private final TwoLayersDrawable twoLayers;
@@ -15,7 +18,7 @@ public class PopUpDrawable extends FixedLayoutDrawable<TwoLayersDrawable> {
             blurrableBackground,
             popUpTextBox
                 .center().crop()
-                .fixSize(new DrawableSize(30, 50))
+                .fixSize(new DrawableSize(8, 20))
                 .addBorderBox()
                 .center().crop()
         );
@@ -25,7 +28,44 @@ public class PopUpDrawable extends FixedLayoutDrawable<TwoLayersDrawable> {
 
     public void displayPopUp(String text) {
         blurrableBackground.blur(true);
-        popUpTextBox.text(text);
+
+        StringBuilder justifiedText = new StringBuilder();
+
+        List<String> line = new ArrayList<>();
+        int lineLength = 0;
+
+        String[] words = text.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            line.add(words[i]);
+            // The first word doesn't need space.
+            lineLength += (lineLength == 0 ? 0 : 1) + words[i].length();
+
+            // +1 is needed to account for the space.
+            if (i == words.length - 1 || lineLength + 1 + words[i + 1].length() > 20) {
+                // We want at least line.size() - 1 spaces.
+                int remainingSpaces = 20 - line.stream().mapToInt(String::length).sum()
+                    - (line.size() - 1);
+
+                for (int j = 0; j < line.size() - 1; j++) {
+                    int numOfSpaces;
+                    if (j == line.size() - 2) {
+                        numOfSpaces = 1 + remainingSpaces;
+                        remainingSpaces = 0;
+                    } else {
+                        numOfSpaces = 1 + remainingSpaces / (line.size() - 1 - j);
+                        remainingSpaces -= numOfSpaces;
+                    }
+
+                    justifiedText.append(line.get(j)).append(" ".repeat(numOfSpaces));
+                }
+                justifiedText.append(line.get(line.size() - 1)).append("\n");
+
+                line.clear();
+                lineLength = 0;
+            }
+        }
+
+        popUpTextBox.text(justifiedText.toString());
         twoLayers.showForeground();
     }
 
