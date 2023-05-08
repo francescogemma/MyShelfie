4c44c6d751b779c3c4c67b8a7f9573e6a49b92db
Cristiano Migali <cristiano.migali@gmail.com>
2023-05-08 17:03:09 +0200

    Fixed bug in pop-up queue disabling: enabled remained always to true

--- src/main/java/it/polimi/ingsw/view/popup/PopUpQueue.java
+++ src/main/java/it/polimi/ingsw/view/popup/PopUpQueue.java
@@ -88,6 +88,7 @@ public class PopUpQueue {
                                 }
 
                                 toDisable = false;
+                                enabled = false;
 
                                 lock.notifyAll();
 
