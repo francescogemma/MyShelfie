0421f9c39036a8cbb04494e4314d980c01183d0c
Cristiano Migali <cristiano.migali@gmail.com>
2023-05-12 17:52:51 +0200

    Add JavaDoc documentation to EventListener

--- src/main/java/it/polimi/ingsw/event/receiver/EventListener.java
+++ src/main/java/it/polimi/ingsw/event/receiver/EventListener.java
@@ -2,6 +2,19 @@ package it.polimi.ingsw.event.receiver;
 
 import it.polimi.ingsw.event.data.EventData;
 
+/**
+ * Represents the callback that gets invoked by an {@link EventReceiver} when it receives an event of
+ * the specified type.
+ *
+ * @param <T> is the type of the events that the listener handles.
+ *
+ * @author Cristiano Migali
+ */
 public interface EventListener<T extends EventData> {
+    /**
+     * Callback invoked by an {@link EventReceiver} when it receives an event of the specified type.
+     *
+     * @param data is the EventData subclass associated with the received event.
+     */
     void handle(T data);
 }
