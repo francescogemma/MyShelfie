1a56cda8be98603dc7c1f4896fde55eadfe015a7
Cristiano Migali <cristiano.migali@gmail.com>
2023-05-12 17:56:23 +0200

    Add JavaDoc documentation to EventTransmitter

--- src/main/java/it/polimi/ingsw/event/transmitter/EventTransmitter.java
+++ src/main/java/it/polimi/ingsw/event/transmitter/EventTransmitter.java
@@ -2,6 +2,19 @@ package it.polimi.ingsw.event.transmitter;
 
 import it.polimi.ingsw.event.data.EventData;
 
+/**
+ * Represent an object which can broadcast {@link EventData}. A {@link EventTransmitter} is coupled with an
+ * {@link it.polimi.ingsw.event.receiver.EventReceiver} which receives the broadcast event and notifies all
+ * its listeners. Event broadcast can be local (a component notifies another on the same machine) or through
+ * network (allows communication between objects "living" on different machines).
+ *
+ * @author Cristiano Migali
+ */
 public interface EventTransmitter {
+    /**
+     * Broadcasts the given {@link EventData} to the coupled {@link it.polimi.ingsw.event.receiver.EventReceiver}.
+     *
+     * @param data is the {@link EventData} which will be broadcast.
+     */
     void broadcast(EventData data);
 }
