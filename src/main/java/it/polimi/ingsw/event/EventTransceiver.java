904e78a0bd002ca2517694dfbfe6cec6ba5c9c56
Cristiano Migali <cristiano.migali@gmail.com>
2023-05-12 18:10:50 +0200

    Add JavaDoc documentation to EventTransceiver

--- src/main/java/it/polimi/ingsw/event/EventTransceiver.java
+++ src/main/java/it/polimi/ingsw/event/EventTransceiver.java
@@ -4,5 +4,13 @@ import it.polimi.ingsw.event.data.EventData;
 import it.polimi.ingsw.event.receiver.EventReceiver;
 import it.polimi.ingsw.event.transmitter.EventTransmitter;
 
+/**
+ * Represents an object which can both broadcast and receive {@link EventData}. That is an object which is
+ * both an {@link EventTransmitter} and an {@link EventReceiver}.
+ * Usually different instances of the same concrete EventTransceiver are coupled: the events broadcast by one are
+ * received by the other and vice-versa.
+ *
+ * @author Giacomo Groppi
+ */
 public interface EventTransceiver extends EventTransmitter, EventReceiver<EventData> {
 }
