008e272256896fe37c28faf3b23294fe4810d84d
Cristiano Migali <cristiano.migali@gmail.com>
2023-05-19 18:16:46 +0200

    Add JavaDoc documentation to NetworkEventTransceiver

--- src/main/java/it/polimi/ingsw/event/NetworkEventTransceiver.java
+++ src/main/java/it/polimi/ingsw/event/NetworkEventTransceiver.java
@@ -20,13 +20,50 @@ import jdk.jfr.Event;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Is an {@link EventTransceiver} which allows broadcast and reception of events through a network. It relies on a
+ * {@link Connection} (whose concrete implementations are {@link it.polimi.ingsw.networking.TCP.TCPConnection} and
+ * {@link it.polimi.ingsw.networking.RMI.RMIConnection}, respectively the former allows communication over TCP protocol,
+ * the latter over RMI protocol). Every NetworkEventTransceiver on an host is coupled with another
+ * NetworkEventTransceiver (usually on a different host), these two have both a {@link Connection} object, every
+ * {@link Connection} represents one of the two ends of the communication channel between the hosts.
+ * When we broadcast events from one NetworkEventTransceiver, the other will receive them and notify all its
+ * registered listeners.
+ *
+ * @author Cristiano Migali
+ */
 public class NetworkEventTransceiver implements EventTransceiver {
+    /**
+     * Lock object used to synchronize listeners registration, removal and event handling.
+     */
     private final Object lock;
 
+    /**
+     * Is the list of listeners registered on the transceiver.
+     */
     private final List<EventListener<EventData>> listeners = new ArrayList<>();
+
+    /**
+     * Is the underlying {@link Connection} object which allows to communicate with the paired NetworkEventTransceiver
+     * on a different host.
+     */
     private final Connection connection;
+
+    /**
+     * {@link Gson} object which allows for serialization of {@link EventData}s before sending them through the
+     * {@link Connection}.
+     */
     private final Gson gson;
 
+    /**
+     * Constructor of the class. Initializes the inner lock, the connection object, the {@link Gson} object
+     * used for serialization and starts the "receiver Thread" which receives events from the coupled
+     * NetworkEventTransceiver and notifies all the listeners.
+     *
+     * @param connection is the connection on which broadcast events will be sent and from which events
+     *                   dispatched by the coupled NetworkEventTransceiver will be received.
+     * @param lock is the inner lock used for listeners registration, removal and events handling.
+     */
     public NetworkEventTransceiver(Connection connection, Object lock) {
         this.lock = lock;
 
@@ -53,12 +90,18 @@ public class NetworkEventTransceiver implements EventTransceiver {
                     eventData = gson.fromJson(eventJSON, EventData.class);
                 } catch (JsonParseException e) {
                     // We skip non-valid event JSONs
-                    Logger.writeCritical("Got exception while deserializing " + e.getMessage());
+                    Logger.writeCritical("Got exception while deserializing: " + e.getMessage());
                     continue;
                 }
 
                 synchronized (this.lock) {
                     List<EventListener<EventData>> listenersCopy = new ArrayList<>(listeners);
+                    /* Event handling is done inside the lock because of an optimization:
+                     * every TUI's component listener needs to synchronize on the drawing lock,
+                     * we take the lock one time and then handle the events.
+                     *
+                     * TODO: Maybe we can do handling without the lock and add the synchronize to every listener
+                     */
                     for (EventListener<EventData> listener : listenersCopy) {
                         listener.handle(eventData);
                     }
@@ -67,6 +110,11 @@ public class NetworkEventTransceiver implements EventTransceiver {
         }).start();
     }
 
+    /**
+     * Broadcasts an internal disconnection {@link EventData} to every registered listener.
+     *
+     * @author Giacomo Groppi
+     */
     private void notifyDisconnection () {
         synchronized (lock) {
             List<EventListener<EventData>> listenersCopy = new ArrayList<>(listeners);
@@ -109,6 +157,12 @@ public class NetworkEventTransceiver implements EventTransceiver {
         }
     }
 
+    /**
+     * Disconnects the underlying connection.
+     * This causes the "receiver Thread" to stop.
+     * After having performed a disconnect the NetworkEventTransceiver is
+     * useless: it can't broadcast nor receive events.
+     */
     public void disconnect() {
         connection.disconnect();
     }
