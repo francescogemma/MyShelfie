63f4167248f5b9bcc9abb360b4385f32b386be1b
Giacomo Groppi <giamg01@gmail.com>
2023-05-22 21:15:21 +0200

    Add JoinGameEventData javaDoc documentation

--- src/main/java/it/polimi/ingsw/event/data/client/JoinGameEventData.java
+++ src/main/java/it/polimi/ingsw/event/data/client/JoinGameEventData.java
@@ -10,16 +10,36 @@ import it.polimi.ingsw.event.transmitter.EventTransmitter;
 import java.util.Objects;
 import java.util.function.Function;
 
+/**
+ * This event is used to request the server to join a game. The request
+ * can be made from both the lobby and the menu.
+ *
+ * @see it.polimi.ingsw.controller.servercontroller.MenuController#joinGame(EventTransmitter, String, String)
+ * @see it.polimi.ingsw.controller.servercontroller.GameController#joinGame(String)
+ * @author Giacomo Groppi
+ * */
 public class JoinGameEventData implements EventData {
     public static final String ID = "JOIN_GAME";
 
+    /**
+     * The name of the game you want to join.
+     * */
     private final String gameName;
 
+    /**
+     * Constructor of the class.
+     * @param gameName The name of the game you want to join.
+     * @throws NullPointerException iff gameName is null
+     * */
     public JoinGameEventData(String gameName) {
         Objects.requireNonNull(gameName);
         this.gameName = gameName;
     }
 
+    /**
+     * This method returns the name of the game you want to join.
+     * @return The name of the game you want to join.
+     * */
     public String getGameName() {
         return gameName;
     }
