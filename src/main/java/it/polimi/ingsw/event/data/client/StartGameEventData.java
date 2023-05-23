1e8e6fd43f86cfd341c559316b5eaa2954124b9d
Giacomo Groppi <giamg01@gmail.com>
2023-05-23 16:07:56 +0200

    Add javaDoc documentation to StartGameEventData

--- src/main/java/it/polimi/ingsw/event/data/client/StartGameEventData.java
+++ src/main/java/it/polimi/ingsw/event/data/client/StartGameEventData.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.event.data.client;
 
+import it.polimi.ingsw.controller.servercontroller.GameController;
 import it.polimi.ingsw.event.Requester;
 import it.polimi.ingsw.event.Responder;
 import it.polimi.ingsw.event.data.EventData;
@@ -9,22 +10,35 @@ import it.polimi.ingsw.event.transmitter.EventTransmitter;
 
 import java.util.function.Function;
 
+/**
+ * This event is used to request the server to start the game
+ * for which the player is in the lobby.
+ *
+ * @see it.polimi.ingsw.controller.servercontroller.MenuController#startGame(GameController, String)
+ * @author Giacomo Groppi
+ * */
 public class StartGameEventData implements EventData {
     public static final String ID = "START_GAME";
 
-    public static CastEventReceiver<StartGameEventData> castEventReceiver(EventReceiver<EventData> receiver) {
+    public static CastEventReceiver<StartGameEventData>
+            castEventReceiver(EventReceiver<EventData> receiver)
+    {
         return new CastEventReceiver<>(ID, receiver);
     }
 
-    public static <T extends EventData> Requester<StartGameEventData, T> requester(EventTransmitter transmitter,
-                                                                                  EventReceiver<EventData> receiver,
-                                                                                   Object responsesLock) {
+    public static <T extends EventData> Requester<StartGameEventData, T>
+            requester(EventTransmitter transmitter,
+                      EventReceiver<EventData> receiver,
+                      Object responsesLock)
+    {
         return new Requester<>(ID, transmitter, receiver, responsesLock);
     }
 
-    public static <T extends EventData> Responder<StartGameEventData, T> responder(EventTransmitter transmitter,
-                                                                                  EventReceiver<EventData> receiver,
-                                                                                  Function<StartGameEventData, T> response) {
+    public static <T extends EventData> Responder<StartGameEventData, T>
+            responder(EventTransmitter transmitter,
+                      EventReceiver<EventData> receiver,
+                      Function<StartGameEventData, T> response)
+    {
         return new Responder<>(ID, transmitter, receiver, response);
     }
 
