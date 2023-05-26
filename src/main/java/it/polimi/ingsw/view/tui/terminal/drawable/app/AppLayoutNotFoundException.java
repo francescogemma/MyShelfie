84044b4fac66f1e2ce253db38c95d4457d5835dc
Cristiano Migali <cristiano.migali@gmail.com>
2023-05-26 18:13:01 +0200

    Add JavaDoc documentation to AppLayoutNotFoundException

--- src/main/java/it/polimi/ingsw/view/tui/terminal/drawable/app/AppLayoutNotFoundException.java
+++ src/main/java/it/polimi/ingsw/view/tui/terminal/drawable/app/AppLayoutNotFoundException.java
@@ -1,6 +1,12 @@
 package it.polimi.ingsw.view.tui.terminal.drawable.app;
 
+/**
+ * Exception thrown there is no {@link AppLayout} in the {@link App} with the specified name.
+ */
 public class AppLayoutNotFoundException extends RuntimeException {
+    /**
+     * @param appLayoutName is the specified name, which doesn't correspond to any {@link AppLayout} in the {@link App}-
+     */
     public AppLayoutNotFoundException(String appLayoutName) {
         super(appLayoutName + " not found in the app");
     }
