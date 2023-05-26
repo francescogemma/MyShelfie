fabce48ddf2074a13df1048ded7aee351098f439
Cristiano Migali <cristiano.migali@gmail.com>
2023-05-26 18:14:09 +0200

    Add JavaDoc documentation to NoDataAtLabelException

--- src/main/java/it/polimi/ingsw/view/tui/terminal/drawable/app/NoDataAtLabelException.java
+++ src/main/java/it/polimi/ingsw/view/tui/terminal/drawable/app/NoDataAtLabelException.java
@@ -1,6 +1,12 @@
 package it.polimi.ingsw.view.tui.terminal.drawable.app;
 
+/**
+ * Exception thrown when there is no data with the specified label inside an {@link AppLayoutData}.
+ */
 public class NoDataAtLabelException extends RuntimeException {
+    /**
+     * @param label is the label which doesn't correspond to any data inside the {@link AppLayoutData}.
+     */
     public NoDataAtLabelException(String label) {
         super("There is no data corresponding to " + label);
     }
