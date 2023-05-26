80d4c6526a118a421c769e26da0e68c3a29a0ae2
Cristiano Migali <cristiano.migali@gmail.com>
2023-05-26 18:15:57 +0200

    Add JavaDoc documentation to WrongDataTypeAtLabelException

--- src/main/java/it/polimi/ingsw/view/tui/terminal/drawable/app/WrongDataTypeAtLabelException.java
+++ src/main/java/it/polimi/ingsw/view/tui/terminal/drawable/app/WrongDataTypeAtLabelException.java
@@ -1,6 +1,13 @@
 package it.polimi.ingsw.view.tui.terminal.drawable.app;
 
+/**
+ * Exception thrown when the data with the specified label has a different concrete type than expected.
+ */
 public class WrongDataTypeAtLabelException extends RuntimeException {
+    /**
+     * @param label is the label associated with the desired data.
+     * @param expectedTypeName is the name of the expected concrete type of the desired data.
+     */
     public WrongDataTypeAtLabelException(String label, String expectedTypeName) {
         super("Data at " + label + " is not a " + expectedTypeName);
     }
