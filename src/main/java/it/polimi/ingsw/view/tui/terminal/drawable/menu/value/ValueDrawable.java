c634d7d1313154d22b62276f230f6f70abad2c03
Cristiano Migali <cristiano.migali@gmail.com>
2023-06-21 17:54:47 +0200

    Add JavaDoc documentation to ValueDrawable

--- src/main/java/it/polimi/ingsw/view/tui/terminal/drawable/menu/value/ValueDrawable.java
+++ src/main/java/it/polimi/ingsw/view/tui/terminal/drawable/menu/value/ValueDrawable.java
@@ -2,6 +2,17 @@ package it.polimi.ingsw.view.tui.terminal.drawable.menu.value;
 
 import it.polimi.ingsw.view.tui.terminal.drawable.Drawable;
 
+/**
+ * Represents a {@link Drawable} which allows the user to specify an input value.
+ * This value can be chosen by the user among a set of possibilities or entered from scratch.
+ *
+ * @param <T> is the type (an integer, a string, a boolean) of the value provided by the Drawable.
+ *
+ * @see TextBox
+ * @see Options
+ *
+ * @author Cristiano Migali
+ */
 public abstract class ValueDrawable<T> extends Drawable {
     public abstract T getValue();
 }
