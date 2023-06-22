de639f5efd29b675cb4dba7ed96a02b41c666bcb
Cristiano Migali <cristiano.migali@gmail.com>
2023-06-22 10:44:56 +0200

    Add JavaDoc documentation to OrientedLayout

--- src/main/java/it/polimi/ingsw/view/tui/terminal/drawable/orientedlayout/OrientedLayout.java
+++ src/main/java/it/polimi/ingsw/view/tui/terminal/drawable/orientedlayout/OrientedLayout.java
@@ -10,13 +10,59 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Represents a Drawable obtained by stacking an ordered set of children Drawables with a given {@link Orientation}
+ * (horizontal or vertical). Each child Drawable has a weight which is proportional to the size dimension
+ * in the layout orientation that it should occupy. For example if the layout has a vertical orientation, its children
+ * should have height proportional to their weight, ideally filling all the width of the layout.
+ * Note that the children of an OrientedLayout are {@link Drawable}, not {@link FullyResizableDrawable},
+ * so there is no guarantee that a certain child will occupy an amount of space proportional to its weight.
+ * In particular the layout tries to resize its children according to their weights through {@link Drawable#askForSize(DrawableSize)};
+ * then it retrieves the actual size of the child after the resize, computes the remaining space and tries to split
+ * it among the remaining children still according to their weights.
+ * The order of resize is: the first child in the set (which is on top if the orientation is vertical or on the
+ * left if the orientation is horizontal); the last child in the set (which is at the bottom if the orientation
+ * is vertical or on the right if the orientation is horizontal) and then all the others
+ * children from the second to the second last.
+ *
+ * @see OrientedLayoutElement
+ *
+ * @author Cristiano Migali
+ */
 public class OrientedLayout extends Drawable {
+    /**
+     * It is the orientation of the layout: vertical or horizontal.
+     */
     private final Orientation orientation;
+
+    /**
+     * It is the list of children Drawables with their weights in the layout.
+     */
     private final List<OrientedLayoutElement> elements;
+
+    /**
+     * It is a list of {@link Coordinate} components "parallel" to the orientation of the layout
+     * (that is line component if the orientation is vertical, column component if the orientation is horizontal).
+     * Every component corresponds to the upper left {@link Coordinate} of the ith children in the layout.
+     * There is also one more component at the end for a fictional starting "at the end" of the layout which
+     * simplifies the resizing code.
+     */
     private final List<Integer> elementsOriginParallelComponent;
 
+    /**
+     * It is the index corresponding to the child of the layout which is on focus or -1 if no child is on focus.
+     */
     private int elementOnFocusIndex = -1;
 
+    /**
+     * Constructor of the class.
+     * Initializes a layout with the given orientation and set of children.
+     *
+     * @param orientation is the {@link Orientation} (vertical or horizontal) of the layout by which its children will
+     *                    be stacked.
+     * @param elements is the ordered set of children of the layout, each one is a Drawable with its weight proportional to
+     *                 the occupied space in the layout.
+     */
     public OrientedLayout(Orientation orientation, OrientedLayoutElement... elements) {
         this.orientation = orientation;
         this.elements = new ArrayList<>(Arrays.asList(elements));
@@ -137,6 +183,15 @@ public class OrientedLayout extends Drawable {
         throw new IllegalStateException("There must be an element corresponding to the requested coordinate");
     }
 
+    /**
+     * Tries to focus the specified child of the layout at a desired Coordinate.
+     *
+     * @param elementToFocusIndex is the index of the child of the layout that should be focused; it cal also be
+     *                            -1 or equal to the number of elements, in that case there is no corresponding
+     *                            child to focus.
+     * @param desiredCoordinate is the desired {@link Coordinate} where we want to focus the specified child.
+     * @return true iff the specified child has been focused.
+     */
     private boolean focusElement(int elementToFocusIndex, Coordinate desiredCoordinate) {
         if (elementToFocusIndex < 0 || elementToFocusIndex >= elements.size()
             || elements.get(elementToFocusIndex).getWeight() == 0) {
@@ -146,6 +201,18 @@ public class OrientedLayout extends Drawable {
         return elements.get(elementToFocusIndex).getDrawable().focus(desiredCoordinate);
     }
 
+    /**
+     * Tries to focus the child of the layout which is in the specified direction with respect to the current
+     * child in focus.
+     *
+     * @param direction indicates the direction of the child to focus with respect to the focused one; it can be
+     *                  +1 (which corresponds to down if orientation is vertical or right if orientation is horizontal)
+     *                  or -1 (which corresponds to up if orientation is vertical or left if orientation is horizontal).
+     * @param desiredCoordinate is the desired {@link Coordinate} where we want to focus the next child.
+     * @return true iff the child in the specified direction has been focused.
+     *
+     * @throws IllegalArgumentException iff direction is not +1 or -1.
+     */
     private boolean focusNextElement(int direction, Coordinate desiredCoordinate) {
         if (direction != 1 && direction != -1) {
             throw new IllegalArgumentException("When changing element on focus, direction must be 1 or -1, got: " +
@@ -169,6 +236,18 @@ public class OrientedLayout extends Drawable {
         return false;
     }
 
+    /**
+     * Tries to focus the child of the layout which is in the specified direction with respect to the current child
+     * in focus. In particular the desired focus {@link Coordinate} is the one, inside the
+     * next child, closest to the previous focus {@link Coordinate}.
+     *
+     * @param direction indicates the direction of the child to focus with respect to the focused one; it can be
+     *                  +1 (which corresponds to down if orientation is vertical or right if orientation is horizontal)
+     *                  or -1 (which corresponds to up if orientation is vertical or left if orientation is horizontal).
+     * @return true iff the child in the specified direction has been focused.
+     *
+     * @throws IllegalArgumentException iff direction is not +1 or -1.
+     */
     private boolean focusNextElement(int direction) {
         int previousElementWithNonNullWeightIndex = -1;
 
@@ -286,10 +365,20 @@ public class OrientedLayout extends Drawable {
         ));
     }
 
+    /**
+     * @return the list of children of the layout.
+     */
     public List<OrientedLayoutElement> getElements() {
         return new ArrayList<>(elements);
     }
 
+    /**
+     * Adds an element at the end of the layout (which is at the bottom if the orientation is vertical, on the right
+     * if the orientation is horizontal).
+     *
+     * @param element is the {@link OrientedLayoutElement}, that is a Drawable with a weight, to be added at the end
+     *                of the layout.
+     */
     public void addElement(OrientedLayoutElement element) {
         elements.add(element);
         elementsOriginParallelComponent.add(0);
