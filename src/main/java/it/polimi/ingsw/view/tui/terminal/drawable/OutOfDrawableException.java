b4f196165131288f7ef01cb19746fb7ebdc5fc56
Cristiano Migali <cristiano.migali@gmail.com>
2023-06-22 12:31:54 +0200

    Add JavaDoc documentation to OutOfDrawableException

--- src/main/java/it/polimi/ingsw/view/tui/terminal/drawable/OutOfDrawableException.java
+++ src/main/java/it/polimi/ingsw/view/tui/terminal/drawable/OutOfDrawableException.java
@@ -1,6 +1,18 @@
 package it.polimi.ingsw.view.tui.terminal.drawable;
 
+/**
+ * Exception thrown when we try to get the symbol in a {@link Drawable} at a {@link Coordinate} outside from it.
+ *
+ * @author Cristiano Migali
+ */
 public class OutOfDrawableException extends RuntimeException {
+    /**
+     * Constructor of the class.
+     * Sets the exception message.
+     *
+     * @param size is the {@link DrawableSize} of the Drawable.
+     * @param coordinate is the {@link Coordinate} outside of the Drawable.
+     */
     public OutOfDrawableException(DrawableSize size, Coordinate coordinate) {
         super(coordinate + " is out of " + size);
     }
