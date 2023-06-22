3fffdfaca6634874cc5d6c3b4ef1d224fae61a6f
giacomogroppi <giamg01@gmail.com>
2023-06-22 22:26:19 +0200

    Add JavaDoc documentation to IllegalFlowException

--- src/main/java/it/polimi/ingsw/model/game/IllegalFlowException.java
+++ src/main/java/it/polimi/ingsw/model/game/IllegalFlowException.java
@@ -1,11 +1,23 @@
 package it.polimi.ingsw.model.game;
 
+/**
+ * This exception is thrown when an illegal game flow occurs within a class.
+ * @author Giacomo Groppi
+ */
 public class IllegalFlowException extends Exception {
+    /**
+     * This constructor creates a new instance of {@link IllegalFlowException} with an empty default message.
+     */
     public IllegalFlowException() {
         super();
     }
 
-    public IllegalFlowException(String s) {
-        super(s);
+   /**
+    * Constructs a new instance of {@link IllegalFlowException} with the specified cause message.
+    *
+    * @param cause The message explaining the cause of the exception.
+    */
+    public IllegalFlowException(String cause) {
+        super(cause);
     }
 }
