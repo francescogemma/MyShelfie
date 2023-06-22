15bb9d79fb5622d88cb4c5699a8db667d4daa6c2
Cristiano Migali <cristiano.migali@gmail.com>
2023-06-22 10:53:05 +0200

    Add JavaDoc documentation to FullyResizableOrientedLayoutElement

--- src/main/java/it/polimi/ingsw/view/tui/terminal/drawable/orientedlayout/FullyResizableOrientedLayoutElement.java
+++ src/main/java/it/polimi/ingsw/view/tui/terminal/drawable/orientedlayout/FullyResizableOrientedLayoutElement.java
@@ -2,7 +2,21 @@ package it.polimi.ingsw.view.tui.terminal.drawable.orientedlayout;
 
 import it.polimi.ingsw.view.tui.terminal.drawable.FullyResizableDrawable;
 
+/**
+ * Represents a {@link FullyResizableOrientedLayout} element, that is a {@link FullyResizableDrawable} with an
+ * associated weight proportional to the space that it occupies in the layout.
+ *
+ * @author Cristiano Migali
+ */
 public class FullyResizableOrientedLayoutElement extends OrientedLayoutElement {
+    /**
+     * Constructor of the class.
+     * Associates the FullyResizableDrawable with the given weight.
+     *
+     * @param drawable is the {@link FullyResizableDrawable} to which a weight will be associated.
+     * @param weight is the weight associated to the FullyResizableDrawable; it is proportional to the space that
+     *               the FullyResizableDrawable will occupy in the layout.
+     */
     public FullyResizableOrientedLayoutElement(FullyResizableDrawable drawable, int weight) {
         super(drawable, weight);
     }
