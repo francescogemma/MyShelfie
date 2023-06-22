630c0283292159fdde3ccd99169941d641443341
Cristiano Migali <cristiano.migali@gmail.com>
2023-06-22 18:10:16 +0200

    Add JavaDoc documentation to BookshelfColumnDrawable

--- src/main/java/it/polimi/ingsw/view/tui/BookshelfColumnDrawable.java
+++ src/main/java/it/polimi/ingsw/view/tui/BookshelfColumnDrawable.java
@@ -15,17 +15,59 @@ import java.util.List;
 import java.util.Optional;
 import java.util.function.Consumer;
 
+/**
+ * It is a Drawable which allows to display a single column of the game bookshelf.
+ * The column is realized through a set of square tiles of solid color, stacked vertically and separated through a
+ * border box.
+ * Columns are focusable: the border box turns red when they are on focus; they allow to register a callback which
+ * is triggered when the user presses enter while focusing a column.
+ * Furthermore the column can be masked, allowing to highlight only certain tiles and add to them a foreground number,
+ * while other tiles are blurred. This is useful when we want to display common goals.
+ *
+ * @author Cristiano Migali
+ */
 public class BookshelfColumnDrawable extends Drawable {
+    /**
+     * It is the index of the column in the bookshelf.
+     */
     private final int column;
 
+    /**
+     * It is the list of {@link TileColor}s of the tiles displayed in this column. The first color in the list
+     * corresponds to the tile on top of the column.
+     */
     private final List<TileColor> tileColors;
+
+    /**
+     * It is the list of integers to be displayed on the foreground of the tiles iff the column is being
+     * masked. In particular if the mask number is 0, no number has to be shown on the foreground of the corresponding
+     * tile, and it has to be blurred, otherwise the tile can remain colored and the mask number should be displayed
+     * on foreground.
+     */
     private final List<Integer> maskNumbers;
+
+    /**
+     * It is true iff the column is being masked.
+     */
     private boolean masked = false;
 
+    /**
+     * It is true iff the column is on focus.
+     */
     private boolean onFocus = false;
 
+    /**
+     * It is equivalent to the number of lines (or columns since tiles have a square shape) that a single tile
+     * of the column occupies on the terminal screen.
+     */
     private int tileSide;
 
+    /**
+     * Constructor of the class.
+     * It initializes all the tiles to {@link TileColor#EMPTY}.
+     *
+     * @param column is the index of this column in the bookshelf.
+     */
     public BookshelfColumnDrawable(int column) {
         this.column = column;
 
@@ -144,14 +186,30 @@ public class BookshelfColumnDrawable extends Drawable {
         return Optional.of(getCenter());
     }
 
+    /**
+     * Callback invoked when the user presses enter while the column is on focus.
+     */
     private Consumer<Integer> onselect;
 
+    /**
+     * Allows to set the callback which is invoked when the user presses enter while the column is on focus.
+     *
+     * @param onselect is the callback invoked when the user presses enter while the column in on focus.
+     * @return this BookshelfColumnDrawable after the specified callback has been set.
+     */
     public BookshelfColumnDrawable onselect(Consumer<Integer> onselect) {
         this.onselect = onselect;
 
         return this;
     }
 
+    /**
+     * Colors the tile at the specified row in this column with the required {@link TileColor}.
+     *
+     * @param row is the row number of the tile to be colored.
+     * @param tileColor is the desired color for the tile to be colored.
+     * @return this BookshelfColumnDrawable after the specified tile has been colored.
+     */
     public BookshelfColumnDrawable color(int row, TileColor tileColor) {
         if (!Bookshelf.isRowInsideTheBookshelf(row)) {
             throw new IllegalArgumentException("A bookshelf row must be between 0 and " + (Bookshelf.ROWS - 1) +
@@ -163,6 +221,15 @@ public class BookshelfColumnDrawable extends Drawable {
         return this;
     }
 
+    /**
+     * Allows to set the mask number for the tile at the specified row in this column.
+     *
+     * @param row is the row number of the tile for which we want to specify the mask number.
+     * @param maskNumber is the mask number that will be associated with the specified tile, in particular it should
+     *                   be 0 if you want to blur the tile, greater than 0 if you want to display such a number
+     *                   on the foreground of the tile.
+     * @return this BookshelfColumnDrawable after the mask number has been set for the specified tile.
+     */
     public BookshelfColumnDrawable mask(int row, int maskNumber) {
         if (!Bookshelf.isRowInsideTheBookshelf(row)) {
             throw new IllegalArgumentException("A bookshelf row must be between 0 and " + (Bookshelf.ROWS - 1) +
@@ -174,6 +241,14 @@ public class BookshelfColumnDrawable extends Drawable {
         return this;
     }
 
+    /**
+     * Allows to mask or unmask the column. In particular, if masked is false, mask numbers for column tiles will be
+     * ignored and no tile will be blurred nor numbers will be shown on foreground, otherwise tiles which have mask
+     * number 0 will be blurred, while other will have the mask number displayed on foreground.
+     *
+     * @param masked it must be true iff we want to mask this column as described above.
+     * @return this BookshelfColumnDrawable after it has been masked or unmasked according to the value of maked.
+     */
     public BookshelfColumnDrawable masked(boolean masked) {
         this.masked = masked;
 
