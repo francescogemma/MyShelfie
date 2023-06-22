04272416b9e2700b7e1af6478465f8b180e87698
Cristiano Migali <cristiano.migali@gmail.com>
2023-06-22 12:28:04 +0200

    Add JavaDoc documentation to Orientation

--- src/main/java/it/polimi/ingsw/view/tui/terminal/drawable/Orientation.java
+++ src/main/java/it/polimi/ingsw/view/tui/terminal/drawable/Orientation.java
@@ -1,6 +1,18 @@
 package it.polimi.ingsw.view.tui.terminal.drawable;
 
+/**
+ * Represents the orientation of displayed elements in a terminal screen.
+ *
+ * @author Cristiano Migali
+ */
 public enum Orientation {
+    /**
+     * It is the orientation from left to right.
+     */
     HORIZONTAL,
+
+    /**
+     * It is the orientation from top to bottom.
+     */
     VERTICAL
 }
