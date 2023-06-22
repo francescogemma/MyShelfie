6ebd6c5021447b9e2ab12d04d9a843d5c3268085
Cristiano Migali <cristiano.migali@gmail.com>
2023-06-22 12:21:40 +0200

    Add JavaDoc documentation to FixedSizeDrawable

--- src/main/java/it/polimi/ingsw/view/tui/terminal/drawable/FixedSizeDrawable.java
+++ src/main/java/it/polimi/ingsw/view/tui/terminal/drawable/FixedSizeDrawable.java
@@ -4,9 +4,27 @@ import it.polimi.ingsw.view.tui.terminal.drawable.symbol.Symbol;
 
 import java.util.Optional;
 
+/**
+ * Represents a Drawable which has a fixed size (number of lines and columns occupied on the terminal screen).
+ * It is realized by ignoring the requests of resize (through {@link Drawable#askForSize(DrawableSize)}) to an underlying
+ * FullyResizableDrawable after it has been resized to the desired size.
+ *
+ * @author Cristiano Migali
+ */
 public class FixedSizeDrawable extends Drawable {
+    /**
+     * It is the underlying FullyResizableDrawable which won't receive resize requests (through {@link Drawable#askForSize(DrawableSize)})
+     * anymore.
+     */
     private final FullyResizableDrawable toFixSize;
 
+    /**
+     * Constructor of the class.
+     * It initializes the underlying FullyResizableDrawable whose size will be fixed.
+     *
+     * @param toFixSize is the underlying Drawable whose size will be fixed.
+     * @param size is the size the underlying FullyResizableDrawable will be resized to.
+     */
     public FixedSizeDrawable(FullyResizableDrawable toFixSize, DrawableSize size) {
         this.toFixSize = toFixSize;
         this.size = size;
