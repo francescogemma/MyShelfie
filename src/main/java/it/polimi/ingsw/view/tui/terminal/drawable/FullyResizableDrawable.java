c66aeb5022e8ee2353c1de3803d16166a57d31cf
Cristiano Migali <cristiano.migali@gmail.com>
2023-06-22 12:26:06 +0200

    Add JavaDoc documentation to FullyResizableDrawable

--- src/main/java/it/polimi/ingsw/view/tui/terminal/drawable/FullyResizableDrawable.java
+++ src/main/java/it/polimi/ingsw/view/tui/terminal/drawable/FullyResizableDrawable.java
@@ -2,6 +2,13 @@ package it.polimi.ingsw.view.tui.terminal.drawable;
 
 import it.polimi.ingsw.view.tui.terminal.drawable.orientedlayout.FullyResizableOrientedLayoutElement;
 
+/**
+ * Represents a Drawable which can resize exactly to every requested size through {@link Drawable#askForSize(DrawableSize)}.
+ * That is, after a call to {@link Drawable#askForSize(DrawableSize)} with a certain size, we are guaranteed that
+ * the DrawableSize will be equal to the requested one.
+ *
+ * @author Cristiano Migali
+ */
 public abstract class FullyResizableDrawable extends Drawable {
     @Override
     public void askForSize(DrawableSize desiredSize) {
@@ -13,6 +20,11 @@ public abstract class FullyResizableDrawable extends Drawable {
         return new FullyResizableOrientedLayoutElement(this, weight);
     }
 
+    /**
+     * @param size is the {@link DrawableSize} this FullyResizableDrawable will be fixed to.
+     * @return a {@link FixedSizeDrawable} whose underlying FullyResizableDrawable is this one, which now has
+     * the size specified through size.
+     */
     public FixedSizeDrawable fixSize(DrawableSize size) {
         return new FixedSizeDrawable(this, size);
     }
