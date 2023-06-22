45dccff51ffca1b617c49f4abd61f74f6dd5ea4f
giacomogroppi <giamg01@gmail.com>
2023-06-22 22:28:55 +0200

    Add JavaDoc documentation to NoPlayerConnectedException

--- src/main/java/it/polimi/ingsw/model/game/NoPlayerConnectedException.java
+++ src/main/java/it/polimi/ingsw/model/game/NoPlayerConnectedException.java
@@ -1,6 +1,13 @@
 package it.polimi.ingsw.model.game;
 
-public class NoPlayerConnectedException extends Exception{
+/**
+ * This exception is thrown when there are no connected users
+ * @author Giacomo Groppi
+ */
+public class NoPlayerConnectedException extends Exception {
+    /**
+     * Constructs a new instance of {@link NoPlayerConnectedException} with a default empty message.
+     */
     public NoPlayerConnectedException () {
         super();
     }
