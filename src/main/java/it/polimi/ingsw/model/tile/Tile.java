aaa28159e1f9eea0f1ccedc6ec7326b330a9da88
Cristiano Migali <cristiano.migali@gmail.com>
2023-06-23 15:40:39 +0200

    Add JavaDoc documentation to Tile and TileVersion

--- src/main/java/it/polimi/ingsw/model/tile/Tile.java
+++ src/main/java/it/polimi/ingsw/model/tile/Tile.java
@@ -1,19 +1,44 @@
 package it.polimi.ingsw.model.tile;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 
+/**
+ * It represents a tile in the game.
+ * Every tile is characterized by a color and a version.
+ * For every color (except for the empty tile) there are 3 version, each one corresponds to a different foreground
+ * image on the tile.
+ * This class implements a singleton pattern, there is only one instance of Tile with a given color and version
+ * couple.
+ *
+ * @author Cristiano Migali
+ */
 public class Tile {
+    /**
+     * It is the color of this tile.
+     */
     private final TileColor color;
+
+    /**
+     * It is the version of this tile, it indicates the foreground image on the tile.
+     */
     private final TileVersion version;
 
+    /**
+     * Constructor of the class.
+     * It initializes the Tile color and version.
+     *
+     * @param tileColor is the color of the background of the tile.
+     * @param tileVersion is the version which indicates the foreground image on the tile.
+     */
     private Tile(TileColor tileColor, TileVersion tileVersion) {
         color = tileColor;
         version = tileVersion;
     }
 
+    /**
+     * Matrix used to store all the instances of Tile to implement a singleton pattern.
+     */
     private static final Tile[][] INSTANCES = new Tile[TileColor.values().length][];
 
     static {
@@ -31,12 +56,28 @@ public class Tile {
         }
     }
 
+    /**
+     * @return all the Tile instances in an array.
+     *
+     * @author Giacomo Groppi
+     */
     public static Collection<Tile> getTiles() {
         return Arrays.stream(INSTANCES)
                 .flatMap(Arrays::stream)
                 .toList();
     }
 
+    /**
+     * Allows to retrieve the instance of the Tile with the specified color and version.
+     * This is the only way of doing so, indeed this class implements a singleton pattern.
+     *
+     * @param tileColor is the background color of the tile.
+     * @param tileVersion is the version which indicates the foreground image on the tile.
+     * @return the instance of the tile with the specified color and version.
+     *
+     * @throws IllegalArgumentException if we try to retrieve the instance of the empty tile specifying
+     * a version which is not the first.
+     */
     public static Tile getInstance(TileColor tileColor, TileVersion tileVersion) {
         if (tileColor == TileColor.EMPTY) {
             if (tileVersion != TileVersion.FIRST) {
@@ -49,17 +90,32 @@ public class Tile {
         return INSTANCES[tileColor.ordinal()][tileVersion.ordinal()];
     }
 
+    /**
+     * @return the background color of this tile.
+     */
     public TileColor getColor() {
         return color;
     }
 
+    /**
+     * @return the version which indicates the foreground image on the tile.
+     */
     public TileVersion getVersion() {
         return version;
     }
 
+    /**
+     * It is the number of different (accounting for versions) tiles which are not empty.
+     */
     public static final int NUM_OF_DIFFERENT_NON_EMPTY_TILES = (TileColor.values().length - 1) *
         TileVersion.values().length;
 
+    /**
+     * Converts a tile which is not empty to a unique index.
+     *
+     * @param tile is the Tile that we want to convert to an index.
+     * @return the index associated with the provided tile.
+     */
     public static int nonEmptyTileToIndex(Tile tile) {
         if (tile.color == TileColor.EMPTY) {
             throw new IllegalArgumentException("Conversion of empty tile to index is not supported");
@@ -69,6 +125,12 @@ public class Tile {
             tile.version.ordinal();
     }
 
+    /**
+     * Converts an index to a non-empty tile.
+     *
+     * @param index is the index for which we want to retrieve the corresponding non-empty tile.
+     * @return the Tile associated with the provided index.
+     */
     public static Tile indexToNonEmptyTile(int index) {
         if (index < 0 || index >= NUM_OF_DIFFERENT_NON_EMPTY_TILES) {
             throw new IllegalArgumentException("index must be between 0 and " + (NUM_OF_DIFFERENT_NON_EMPTY_TILES - 1)
--- src/main/java/it/polimi/ingsw/model/tile/TileVersion.java
+++ src/main/java/it/polimi/ingsw/model/tile/TileVersion.java
@@ -1,7 +1,23 @@
 package it.polimi.ingsw.model.tile;
 
+/**
+ * Indicates the version of a {@link Tile} which corresponds to its foreground image.
+ *
+ * @author Cristiano Migali
+ */
 public enum TileVersion {
+    /**
+     * It is the first version of a tile.
+     */
     FIRST,
+
+    /**
+     * It is the second version of a tile.
+     */
     SECOND,
+
+    /**
+     * It is the third version of a tile.
+     */
     THIRD
 }
