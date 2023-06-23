c9919f0b2abba422531cb26f5bfd7fc2e10118e6
Giacomo Groppi <giamg01@gmail.com>
2023-06-23 21:17:27 +0200

    Add javadoc documentation to CommonGoalCompletedEventData

--- src/main/java/it/polimi/ingsw/event/data/game/CommonGoalCompletedEventData.java
+++ src/main/java/it/polimi/ingsw/event/data/game/CommonGoalCompletedEventData.java
@@ -11,14 +11,40 @@ import it.polimi.ingsw.model.game.Player;
 
 import java.util.function.Function;
 
+/**
+ * This event indicates that a player has completed a common goal.
+ * @author Giacomo Groppi
+ * */
 public class CommonGoalCompletedEventData implements EventData {
+    /**
+     * The {@link BookshelfMaskSet} of the goal completed.
+     * */
     private final BookshelfMaskSet maskSet;
+
+    /**
+     * The {@link Player} that completed the goal.
+     */
     private final Player player;
+
+    /**
+     * The index of the common goal completed.
+     * */
     private final int commonGoalCompleted;
+
+    /**
+     * The points earned by the player.
+     * */
     private final int points;
 
     public static final String ID = "COMMON_GOAL_COMPLETED";
 
+    /**
+     * Constructor of the event.
+     * @param player The {@link Player} that completed the goal.
+     * @param points The points earned by the player.
+     * @param maskSet The {@link BookshelfMaskSet} of the goal completed.
+     * @param commonGoalCompleted The index of the common goal completed.
+     * */
     public CommonGoalCompletedEventData(Player player, int points, BookshelfMaskSet maskSet, int commonGoalCompleted) {
         this.player = player;
         this.maskSet = maskSet;
@@ -26,18 +52,34 @@ public class CommonGoalCompletedEventData implements EventData {
         this.points = points;
     }
 
+    /**
+     * Getter for the points earned by the player.
+     * @return The points earned by the player.
+     * */
     public int getPoints () {
         return points;
     }
 
+    /**
+     * Getter for the {@link BookshelfMaskSet} of the goal completed.
+     * @return The {@link BookshelfMaskSet} of the goal completed.
+     * */
     public BookshelfMaskSet getBookshelfMaskSet() {
         return this.maskSet;
     }
 
+    /**
+     * Getter for the {@link Player} that completed the goal.
+     * @return The {@link Player} that completed the goal.
+     * */
     public Player getPlayer() {
-        return this.player;
+        return new Player(player);
     }
 
+    /**
+     * Getter for the index of the common goal completed.
+     * @return The index of the common goal completed.
+     * */
     public int getCommonGoalCompleted () {
         return this.commonGoalCompleted;
     }
