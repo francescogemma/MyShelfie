5fe11e5d1396de0be2831d53de737f529a67bcc5
Cristiano Migali <cristiano.migali@gmail.com>
2023-06-23 17:31:16 +0200

    Add JavaDoc documentation to PopUp and PopUpQueue

--- src/main/java/it/polimi/ingsw/view/popup/PopUp.java
+++ src/main/java/it/polimi/ingsw/view/popup/PopUp.java
@@ -5,19 +5,77 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.function.Consumer;
 
+/**
+ * Represents a pop-up which can be displayed on a user interface.
+ * A pop-up is characterized by a text message which will be displayed on screen and a kind which allows
+ * to group together pop-ups that inform the user of different happenings of the same event.
+ * Furthermore it is possible to add some data (in the form of an Object) to the pop-up.
+ *
+ * @see PopUpQueue
+ *
+ * @author Cristiano Migali
+ */
 public class PopUp {
+    /**
+     * It is the lock on which the pop-up queue which created this pop-up synchronizes.
+     * The pop-up synchronizes on this lock when it performs the hide request to the queue.
+     * In fact the pop-up has no reference to its queue.
+     * This is the only way of hiding a pop-up.
+     * In this way the component which have access to the {@link PopUpQueue} can't directly hide the pop-ups.
+     *
+     * @see PopUpQueue
+     */
     private final Object queueLock;
 
+    /**
+     * It is the kind of the pop-up which allows to group it with other pop-ups that inform the user of different
+     * happenings of the event associated with this pop-up.
+     */
     private final String kind;
+
+    /**
+     * It is the text message of the pop-up.
+     */
     private final String text;
+
+    /**
+     * It is the data Object eventually attached to the pop-up.
+     */
     private final Object data;
 
+    /**
+     * It is true iff the pop-up has still to be displayed on the interface.
+     */
     private boolean toShow = true;
+
+    /**
+     * It is true iff the pop-up should be hidden from the interface.
+     */
     private boolean toHide = false;
 
+    /**
+     * Callback invoked when the pop-up is shown on the interface.
+     */
     private final Consumer<PopUp> onshow;
+
+    /**
+     * Callback invoked when the pop-up is hidden from the interface.
+     */
     private final Consumer<PopUp> onhide;
 
+    /**
+     * Constructor of the class.
+     * It initializes all the required attributes.
+     *
+     * @param queueLock it is the lock object on which the {@link PopUpQueue} that is creating this pop-up
+     *                  synchronizes.
+     * @param kind is a string which allows to group together pop-ups that inform the user of different happenings of
+     *             the same event.
+     * @param text is the text message of the pop-up.
+     * @param data is the data Object which can be associated to the pop-up.
+     * @param onshow is the callback invoked when the pop-up is displayed on the interface.
+     * @param onhide is the callback invoked when the pop-up is hidden from the interface.
+     */
     public PopUp(Object queueLock, String kind, String text, Object data,
                  Consumer<PopUp> onshow, Consumer<PopUp> onhide) {
         this.queueLock = queueLock;
@@ -29,14 +87,24 @@ public class PopUp {
         this.onhide = onhide;
     }
 
+    /**
+     * @return the text message of this pop-up.
+     */
     public String getText() {
         return text;
     }
 
+    /**
+     * @return an Optional which contains the data Object associated with this pop-up if it has one, an empty Optional
+     * otherwise.
+     */
     public Optional<Object> getData() {
         return Optional.ofNullable(data);
     }
 
+    /**
+     * Asks the {@link PopUpQueue} to hide this pop-up from the interface. This is the onmly way of doing so.
+     */
     public void askToHide() {
         synchronized (queueLock) {
             toHide = true;
@@ -45,33 +113,63 @@ public class PopUp {
         }
     }
 
+    /**
+     * @return an Optional with the kind of this pop-up if it has one, an empty Optional otherwise.
+     */
     Optional<String> getKind() {
         return Optional.ofNullable(kind);
     }
 
+    /**
+     * @return true iff this pop-up has to be shown on the interface.
+     */
     boolean isToShow() {
         return toShow;
     }
+
+    /**
+     * @return true iff this pop-up has to be hidden from the interface.
+     */
     boolean isToHide() {
         return toHide;
     }
 
+    /**
+     * Allows to set that this pop-up has been shown on the interface.
+     */
     void setShown() {
         toShow = false;
     }
 
+    /**
+     * Allows to set that this pop-up has been hidden from the interface.
+     */
     void setHidden() {
         toHide = false;
     }
 
+    /**
+     * Invokes the callback which gets notified when the pop-up has been shown on the interface.
+     */
     void onshow() {
         onshow.accept(this);
     }
 
+    /**
+     * Invokes the callback which gets notified when the pop-up has been hidden from the interface.
+     */
     void onhide() {
         onhide.accept(this);
     }
 
+    /**
+     * Default callback that should be invoked when the pop-up has been shown to hide it after a fixed time interval.
+     *
+     * @param milliseconds is the number of milliseconds of the time interval after which we want to hide the pop-up
+     *                     from the interface.
+     * @return the callback which, if invoked when the pop-up has been shown, hides the pop-up after the specified
+     * fixed time interval.
+     */
     public static Consumer<PopUp> hideAfter(long milliseconds) {
         return popUp -> {
             new Timer().schedule(new TimerTask() {
--- src/main/java/it/polimi/ingsw/view/popup/PopUpQueue.java
+++ src/main/java/it/polimi/ingsw/view/popup/PopUpQueue.java
@@ -4,15 +4,54 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
+/**
+ * Represents a queue of {@link PopUp}s which allows to display them on a certain interface.
+ * In particular the user can specify a function to display a {@link PopUp} on a certain interface and another function
+ * to hide it.
+ * Furthermore the user can add pop-ups to the queue.
+ * The PopUpQueue is responsible for calling the function to show and hide pop-ups at the right time.
+ * Each pop-up has some callbacks which can be defined by the user to control when the pop-up is hidden from the queue.
+ *
+ * @see PopUp
+ *
+ * @author Cristiano Migali
+ */
 public class PopUpQueue {
+    /**
+     * Lock object on which the queue synchronizes when adding or removing pop-ups.
+     */
     private final Object lock;
 
+    /**
+     * Function which displays a pop-up on a certain interface.
+     */
     private final Consumer<String> showPopUp;
+
+    /**
+     * Function which hides a pop-up from a certain interface.
+     */
     private final Runnable hidePopUp;
 
+    /**
+     * It is true iff pop-up can be added to the queue.
+     */
     private boolean enabled = true;
+
+    /**
+     * It is the list of pop-ups in the queue. The first pop-up in the list is the first that will be shown or it
+     * is the one that is being shown.
+     */
     private final List<PopUp> popUps = new ArrayList<>();
 
+    /**
+     * Constructor of the class.
+     * it starts the thread responsible for showing and hiding pop-ups.
+     *
+     * @param showPopUp is the function which displays a pop-up on the desired interface.
+     * @param hidePopUp is the function which hides a pop-up from the desired interface.
+     * @param lock is the lock object on which this queue synchronizes while showing or hiding pop-ups, adding or removing
+     *             them.
+     */
     public PopUpQueue(Consumer<String> showPopUp, Runnable hidePopUp, Object lock) {
         this.showPopUp = showPopUp;
         this.hidePopUp = hidePopUp;
@@ -57,6 +96,11 @@ public class PopUpQueue {
         }).start();
     }
 
+    /**
+     * Adds a pop-up at the end of the queue.
+     *
+     * @param newPopUp is the pop-up that will be added at the end of the queue.
+     */
     private void add(PopUp newPopUp) {
         if (!enabled) {
             throw new IllegalStateException("You can't add pop-ups to the pop-up queue while it is disabled");
@@ -75,30 +119,75 @@ public class PopUpQueue {
         lock.notifyAll();
     }
 
+    /**
+     * Adds a pop-up with the provided text message and callbacks invoked respectively when the pop-up is shown
+     * and hidden on the interface.
+     *
+     * @param text is the text message of the pop-up that has to be added to the queue.
+     * @param onshow is the callback which is invoked when the pop-up added to the queue is shown on the interface.
+     * @param onhide is the callback which is invoked when the pop-up added to the queue is hidden from the interface.
+     */
     public void add(String text, Consumer<PopUp> onshow, Consumer<PopUp> onhide) {
         synchronized (lock) {
             add(new PopUp(lock, null, text, null, onshow, onhide));
         }
     }
 
+    /**
+     * Adds a pop-up with the provided text message, data object and callbacks invoked respectively when the pop-up
+     * is shown and hidden on the interface.
+     *
+     * @param text is the text message of the pop-up that has to be added to the queue.
+     * @param data is the data Object associated with the pop-up that has to be added to the queue.
+     * @param onshow is the callback which is invoked when the pop-up added to the queue is shown on the interface.
+     * @param onhide is the callback which is invoked when the pop-up added to the queue is hidden from the interface.
+     */
     public void add(String text, Object data, Consumer<PopUp> onshow, Consumer<PopUp> onhide) {
         synchronized (lock) {
             add(new PopUp(lock, null, text, data, onshow, onhide));
         }
     }
 
+    /**
+     * Adds a pop-up with the provided text message, kind and callbacks invoked respectively when the pop-up
+     * is shown and hidden on the interface.
+     *
+     * @param text is the text message of the pop-up that has to be added to the queue.
+     * @param kind is the kind of the pop-up that has to be added to the queue. It allows to group together pop-ups
+     *             which inform the user of different happenings of the same event.
+     * @param onshow is the callback which is invoked when the pop-up added to the queue is shown on the interface.
+     * @param onhide is the callback which is invoked when the pop-up added to the queue is hidden from the interface.
+     *
+     * @see PopUp Pop-up kind
+     */
     public void add(String text, String kind, Consumer<PopUp> onshow, Consumer<PopUp> onhide) {
         synchronized (lock) {
             add(new PopUp(lock, kind, text, null, onshow, onhide));
         }
     }
 
+    /**
+     * Adds a pop-up with the provided text message, kind, data object and callbacks invoked respectively when
+     * the pop-up is shown and hidden on the interface.
+     *
+     * @param text is the text message of the pop-up that has to be added to the queue.
+     * @param kind is the kind of the pop-up that has to be added to the queue. It allows to group together pop-ups
+     *             which inform the user of different happenings of the same event.
+     * @param data is the data Object associated with the pop-up that has to be added to the queue.
+     * @param onshow is the callback which is invoked when the pop-up added to the queue is shown on the interface.
+     * @param onhide is the callback which is invoked when the pop-up added to the queue is hidden from the interface.
+     */
     public void add(String text, String kind, Object data, Consumer<PopUp> onshow, Consumer<PopUp> onhide) {
         synchronized (lock) {
             add(new PopUp(lock, kind, text, data, onshow, onhide));
         }
     }
 
+    /**
+     * Disables the queue. After this method has been invoked, no pop-up can be added to the queue.
+     * The queue should be thrown away and substituted with a new one.
+     * This method stops the thread responsible for showing and hiding pop-ups.
+     */
     public void disable() {
         synchronized (lock) {
             if (!enabled) {
