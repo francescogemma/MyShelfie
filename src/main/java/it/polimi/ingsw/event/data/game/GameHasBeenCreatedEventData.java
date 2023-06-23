20d4c1e9dd8d836235631e4a6db01b394d6f13cc
Giacomo Groppi <giamg01@gmail.com>
2023-06-23 21:33:08 +0200

    Add javaDoc documentation to GameHasBeenCreatedEventData

--- src/main/java/it/polimi/ingsw/event/data/game/GameHasBeenCreatedEventData.java
+++ src/main/java/it/polimi/ingsw/event/data/game/GameHasBeenCreatedEventData.java
@@ -12,16 +12,47 @@ import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
 
+/**
+ * This event indicates that a game has been created.
+ * All the game contains in the event are accessible by the player who received the event.
+ * @author Giacomo Groppi
+ */
 public class GameHasBeenCreatedEventData implements EventData {
+    /**
+     * The list of the available games.
+     */
     private final List<AvailableGame> games;
     public static final String ID = "GAME_HAS_BEEN_CREATED";
 
+    /**
+     * This class represents a game that is available to join for
+     * the player who received the event
+     */
     public static class AvailableGame {
+        /**
+         * The owner of the game.
+         */
         private final String owner;
+
+        /**
+         * The name of the game.
+         */
         private final String name;
+
+        /**
+         * Indicates if the game has started.
+         */
         private final boolean isStarted;
+
+        /**
+         * Indicates if the game has stopped.
+         */
         private final boolean isStopped;
 
+        /**
+         * Creates a new AvailableGame object from a GameView object.
+         * @param gameView the game that the user can join
+         */
         public AvailableGame(GameView gameView) {
             this.owner = gameView.getOwner();
             this.name = gameView.getName();
@@ -29,18 +60,32 @@ public class GameHasBeenCreatedEventData implements EventData {
             this.isStopped = gameView.isStopped();
         }
 
+        /**
+         * Getter for the owner of the game.
+         * @return The owner of the game.
+         */
         public String owner() {
             return owner;
         }
 
+        /**
+         * Getter for the name of the game.
+         * @return The name of the game.
+         * */
         public String name() {
             return name;
         }
 
+        /**
+         * @return True if the game has started, false otherwise.
+         * */
         public boolean isStarted() {
             return isStarted;
         }
 
+        /**
+         * @return True if the game has stopped, false otherwise.
+         * */
         public boolean isStopped() {
             return isStopped;
         }
@@ -63,11 +108,18 @@ public class GameHasBeenCreatedEventData implements EventData {
         }
     }
 
-
+    /**
+     * Create a new instance of the event.
+     * @param games The list of the available games for the player.
+     * */
     public GameHasBeenCreatedEventData(List<AvailableGame> games) {
         this.games = Collections.unmodifiableList(games);
     }
 
+    /**
+     * Create a new instance of the event.
+     * @param gameView The game that the player can join.
+     * */
     public GameHasBeenCreatedEventData(GameView gameView) {
         this.games = List.of(new AvailableGame(gameView));
     }
@@ -88,6 +140,10 @@ public class GameHasBeenCreatedEventData implements EventData {
         return new Responder<>(ID, transmitter, receiver, response);
     }
 
+    /**
+     * Getter for the list of the available games.
+     * @return The list of the available games.
+     * */
     public List<AvailableGame> getNames () {
         return games;
     }
