34c6f2b181056c9a2776f22261b38f5f0307d30a
Giacomo Groppi <giamg01@gmail.com>
2023-06-23 22:00:44 +0200

    Add JavaDoc documentation to VoidEventData

--- src/main/java/it/polimi/ingsw/event/data/VoidEventData.java
+++ src/main/java/it/polimi/ingsw/event/data/VoidEventData.java
@@ -1,5 +1,9 @@
 package it.polimi.ingsw.event.data;
 
+/**
+ * Represents an EventData which does not carry any information.
+ * @author Giacomo Groppi
+ */
 public class VoidEventData implements EventData {
     public final static String ID = "VOID";
 
