bfc122f2b085cba3d869b5cbe6ef2f177c159ee5
michele-miotti-uni <michele.miotti@mail.polimi.it>
2023-06-25 12:12:32 +0200

    Add full documentation to GameLobbyMenuController

--- src/main/java/it/polimi/ingsw/view/gui/controller/GameLobbyMenuController.java
+++ src/main/java/it/polimi/ingsw/view/gui/controller/GameLobbyMenuController.java
@@ -32,40 +32,120 @@ import javafx.util.Callback;
 
 import java.util.Optional;
 
+/**
+ * This menu is part of the game's graphical use interface. It shows a list of players inside a lobby. All players are
+ * displayed using a ListView object.
+ */
 public class GameLobbyMenuController extends Controller {
+    /**
+     * A text label marking the name of the current game. All players displayed below this label are currently waiting
+     * for this specific game to start.
+     */
     @FXML private Label gameNameLabel;
+
+    /**
+     * A button to navigate to the previous menu, and exit the lobby. It brings the user back to the list of available
+     * games.
+     */
     @FXML private Button backToAvailableGamesButton;
+
+    /**
+     * A button to start the game. If the game is already running, this button is switched with a "resume game" button.
+     */
     @FXML private Button startGameButton;
+
+    /**
+     * A button to resume the game, if the game is already running. If the game is not running, this button is not shown,
+     * and an appropriate "start game" button is displayed instead.
+     */
     @FXML private Button restartGameButton;
+
+    /**
+     * A list containing the name of all players.
+     */
     private ObservableList<String> players;
+
+    /**
+     * The actual JavaFX list object that is rendered on screen: a list of horizontal bars containing a textual string,
+     * with the name of all players that are currently waiting in the lobby.
+     */
     @FXML private ListView<String> playersListView;
 
+    /**
+     * Utility class attribute that stores this menu's name. This attribute is often used by other layouts, to switch to
+     * the next menu without needing to rewrite and consequentially expose its name.
+     */
     public static final String NAME = "GameLobbyMenu";
 
     // Data:
+    /**
+     * The current active transceiver is stored here. This object is initialized in the "initialize" method within this
+     * class.
+     */
     private NetworkEventTransceiver transceiver = null;
 
     // Utilities:
+    /**
+     * Used to inform the server that the current user is trying to access the lobby.
+     */
     private Requester<Response<VoidEventData>, JoinLobbyEventData> joinLobbyRequester = null;
+
+    /**
+     * Used to inform the server that the current user (expected to be the user that created the lobby) is trying to
+     * start the game.
+     */
     private Requester<Response<VoidEventData>, StartGameEventData> startGameRequester = null;
+
+    /**
+     * Used to inform the server that a game that has been paused should be resumed.
+     */
     private Requester<Response<VoidEventData>, RestartGameEventData> restartGameRequester = null;
+
+    /**
+     * If the user exits the menu through the backToAvailableGamesButton, all other users need to be notified of this
+     * event, since their respective list of players in the lobby has to be updated.
+     */
     private Requester<Response<VoidEventData>, ExitLobbyEventData> exitLobbyRequester = null;
 
+    /**
+     * Receives the event relative to a new player entering the lobby.
+     */
     private EventReceiver<PlayerHasJoinLobbyEventData> playerHasJoinLobbyReceiver = null;
+
+    /**
+     * Receives an event informing the client that the game has started. This means that the menu needs to be switched
+     * to the main game's menu.
+     */
     private EventReceiver<GameHasStartedEventData> gameHasStartedReceiver = null;
+
+    /**
+     * Receiver waiting for the event related to another player exiting the lobby. This is needed to update the list view
+     * containing all online players' names.
+     */
     private EventReceiver<PlayerHasExitLobbyEventData> playerHasExitLobbyReceiver = null;
 
     // Listeners:
+
+    /**
+     * Listener that automatically adds the newly entered player's name to the list view, upon arrival.
+     */
     private final EventListener<PlayerHasJoinLobbyEventData> playerHasJoinLobbyListener = data -> {
         Platform.runLater(() -> {
             players.add(data.username());
         });
     };
 
+    /**
+     * Listener that automatically changes the game's layout to the full game menu, after the host decides to start the
+     * game.
+     */
     private final EventListener<GameHasStartedEventData> gameHasStartedListener = data -> {
         switchLayout(GameController.NAME);
     };
 
+    /**
+     * Removes a player from this menu's list view, if that player has just exited the lobby.
+     */
     private final EventListener<PlayerHasExitLobbyEventData> playerHasExitLobbyListener = data -> {
         Platform.runLater(() -> {
             players.remove(data.username());
@@ -73,6 +153,9 @@ public class GameLobbyMenuController extends Controller {
         });
     };
 
+    /**
+     * Helper function to encapsulate the act of starting a new game, using this class' appropriate requester.
+     */
     @FXML
     private void startGame() {
         new Thread(new Task<Void>() {
@@ -89,6 +172,9 @@ public class GameLobbyMenuController extends Controller {
         }).start();
     }
 
+    /**
+     * Helper function to encapsulate the act of resuming a game, using this class' appropriate requester.
+     */
     @FXML
     private void restartGame() {
         new Thread(new Task<Void>() {
@@ -105,6 +191,10 @@ public class GameLobbyMenuController extends Controller {
         }).start();
     }
 
+    /**
+     * Helper function to encapsulate the act of exiting the lobby, using this class' appropriate requester.
+     * This makes the current user exit the lobby: all other players act independently.
+     */
     @FXML
     private void backToAvailableGames() {
         new Thread(new Task<Void>() {
@@ -128,12 +218,19 @@ public class GameLobbyMenuController extends Controller {
         }).start();
     }
 
+    /**
+     * This method is called if the user requests to close the window in any way, and ends the process.
+     */
     @FXML
     private void exit() {
         Platform.exit();
         System.exit(0);
     }
 
+    /**
+     * Set up the menu by adding the game name header, booting all receivers and requesters, and setting a cell
+     * factory to the list view, in order to apply the correct styling and size to its objects.
+     */
     @FXML
     private void initialize() {
         String gameName = (String) getValue("selectedgamename");
