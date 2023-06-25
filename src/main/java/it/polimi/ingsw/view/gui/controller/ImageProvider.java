ac0a72441edeade049e91ba6639051ab5eff2066
michele-miotti-uni <michele.miotti@mail.polimi.it>
2023-06-25 12:25:25 +0200

    Add full documentation to ImageProvider

--- src/main/java/it/polimi/ingsw/view/gui/controller/ImageProvider.java
+++ src/main/java/it/polimi/ingsw/view/gui/controller/ImageProvider.java
@@ -10,15 +10,39 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-
+/**
+ * Utility class to easily retrieve images without needing to constantly reference a directory URL. It's a singleton helper
+ * object, and contains a variety of methods, divided for image category. (tiles, goals, ...)
+ */
 public class ImageProvider {
+    /**
+     * Used for class synchronization and concurrency checks.
+     */
     private static final Object lock = new Object();
+
+    /**
+     * The single instance of this class, stored as a constant class variable to implement the singleton pattern.
+     * This attribute should be accessed to get the main object, without using any constructors, through the getInstance
+     * method.
+     */
     private static final ImageProvider INSTANCE;
+
+    /**
+     * A map containing all required images, indexed with a string consisting in that image's path URL.
+     */
     private final Map<String, Image> images;
+
+    /**
+     * Class constructor that initializes the hashmap.
+     */
     private ImageProvider() {
         images = new HashMap<>();
     }
 
+    /**
+     * Use this method as if it were a constructor. It gets the singleton instance of this class.
+     * @return this class' instance.
+     */
     public static ImageProvider getInstance() {
         return INSTANCE;
     }
@@ -43,6 +67,11 @@ public class ImageProvider {
         }
     }
 
+    /**
+     * Returns a specific personal goal's image, given its index.
+     * @param index is the requested personal goal's image's index.
+     * @return an image representing a personal goal with that index.
+     */
     final Image getPersonalGoal (int index) {
         synchronized (lock) {
             assert index >= 0 && index < 12;
@@ -52,6 +81,11 @@ public class ImageProvider {
         }
     }
 
+    /**
+     * Returns a specific common goal's image, given its index.
+     * @param index is the requested common goal's image's index.
+     * @return an image representing a common goal with that index.
+     */
     final Image getCommonGoal (int index) {
         synchronized (lock) {
             assert index >= 0 && index < 12;
@@ -61,6 +95,11 @@ public class ImageProvider {
         }
     }
 
+    /**
+     * Returns a tile's image, given its relative tile object.
+     * @param tile is the tile for which we need an appropriate image.
+     * @return an image representing the given tile.
+     */
     final Image getTile(Tile tile) {
         synchronized (lock) {
             Objects.requireNonNull(tile);
@@ -70,15 +109,30 @@ public class ImageProvider {
         }
     }
 
+    /**
+     * Utility function to transform a personal goal index to a full path URL.
+     * @param index is the requested personal goal's numerical index.
+     * @return the full path to that personal goal.
+     */
     private static String personalGoalToUrl(int index) {
         if (index > 0) return "/personal goal cards/Personal_Goals" + (index + 1) + ".png";
         return "/personal goal cards/Personal_Goals.png";
     }
 
+    /**
+     * Utility function to transform a common goal index to a full path URL.
+     * @param index is the requested common goal's numerical index.
+     * @return the full path to that common goal.
+     */
     private static String commonGoalToUrl(int index) {
         return "/common goal cards/%s.jpg".formatted(index + 1);
     }
 
+    /**
+     * Utility function to transform a tile to its full path URL.
+     * @param tile is the tile for which we need its image's path URL.
+     * @return the full path to that tile's image.
+     */
     private static String tileToUrl(Tile tile) {
         if(tile == Tile.getInstance(TileColor.GREEN, TileVersion.FIRST)) {
             return "/item tiles/Gatti1.1.png";
