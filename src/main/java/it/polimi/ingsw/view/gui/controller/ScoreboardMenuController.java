c08df71ffa7eea6d6101c09e8f7cb8f9e42d435d
michele-miotti-uni <michele.miotti@mail.polimi.it>
2023-06-25 13:08:09 +0200

    Add full documentation to ScoreboardMenuController

--- src/main/java/it/polimi/ingsw/view/gui/controller/ScoreboardMenuController.java
+++ src/main/java/it/polimi/ingsw/view/gui/controller/ScoreboardMenuController.java
@@ -18,27 +18,60 @@ import javafx.util.Callback;
 
 import java.util.Optional;
 
-
+/**
+ * This menu is part of the game's graphical use interface. It contains an array of player names and scores, ordered by
+ * points, coloured according to a specific standard. It's shown by the end of the game, as a way to sum up the final
+ * game state, and declare a winner.
+ */
 public class ScoreboardMenuController extends Controller {
+    /**
+     * Utility class attribute that stores this menu's name. This attribute is often used by other layouts, to switch to
+     * the next menu without needing to rewrite and consequentially expose its name.
+     */
     public static final String NAME = "ScoreboardMenu";
 
+    /**
+     * Button to go back to the list of available games, after seeing the leaderboard. This button ultimately ends the player's
+     * connection to the game, and brings them back to game selection.
+     */
     @FXML private Button backToAvailableGamesButton;
+
+    /**
+     * An array of player names and scores, ordered by points, coloured according to a specific standard. It's placed
+     * in the middle of the screen, and consists in the scoreboard.
+     */
     @FXML private ListView<DisplayablePlayer> scoreBoardListView;
 
     // Data:
+
+    /**
+     * The current active transceiver is stored here. This object is initialized in the "initialize" method within this
+     * class.
+     */
     private NetworkEventTransceiver transceiver = null;
 
+    /**
+     * Callback method to change the layout to the available games menu.
+     */
     @FXML
     private void backToAvailableGames() {
         switchLayout(AvailableGamesMenuController.NAME);
     }
 
+    /**
+     * This method is called if the user requests to close the window in any way, and ends the process.
+     */
     @FXML
     private void exit() {
         Platform.exit();
         System.exit(0);
     }
 
+    /**
+     * Set up the menu by booting all receivers and requesters, and setting a cell factory to the list view, in order to
+     * apply the correct styling and size to its objects, as well as specific colours that represent the state of a
+     * particular user. (winner, online, offline, ...)
+     */
     @FXML
     private void initialize() {
         if (transceiver == null) {
