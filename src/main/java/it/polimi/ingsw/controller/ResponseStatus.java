1e561250e273a3252f22bb70fd60a5b2903f7c5a
Cristiano Migali <cristiano.migali@gmail.com>
2023-06-26 09:08:49 +0200

    Add JavaDoc documentation to remove warnings from ResponseStatus

--- src/main/java/it/polimi/ingsw/controller/ResponseStatus.java
+++ src/main/java/it/polimi/ingsw/controller/ResponseStatus.java
@@ -1,7 +1,19 @@
 package it.polimi.ingsw.controller;
 
+/**
+ * It is the result status of an operation performed server-side after a synchronous request from the client.
+ *
+ * @author Giacomo Groppi
+ */
 public enum ResponseStatus {
+    /**
+     * The operation has succeeded.
+     */
     SUCCESS,
+
+    /**
+     * The operation has failed.
+     */
     FAILURE;
 
     @Override
